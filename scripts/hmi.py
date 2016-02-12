773807ff4526d0bc3d160581f7d2b080e059cc8a
peteraaser <peteraaser@gmail.com>
2016-02-12 16:35:20 +0100

    Add pwm placeholder message for bypassing directly to arduino

--- scripts/hmi.py
+++ scripts/hmi.py
@@ -3,18 +3,42 @@
 import rospy
 from std_msgs.msg import Int32
 from controller.msg import control
+from controller.msg import pwm_placeholder
+
+    
 
 
-mux_pub = rospy.Publisher('pwm_signal_input', Int32, queue_size=10) 
+class HmiNode:
 
-def controller_callback(data):
-    mux_pub.publish(data.strafe_X)
-    print(data.strafe_X)
+    def __init__(self):
+        self.pwm_signals = pwm_placeholder()
+        self.pwm_publisher = rospy.Publisher('pwm_requests', pwm_placeholder, queue_size=10)
+        rospy.Subscriber('control', control, self.controller_callback)
 
+        rospy.init_node("hmi_node", anonymous=True)
+        self.rate = rospy.Rate(10)
+
+
+    def start_node(self):
+        while not rospy.is_shutdown():
+            rospy.spin()
+
+    def controller_callback(self, control):
+        self.pwm_signals.pwm1 = control.strafe_X
+        self.pwm_signals.pwm2 = control.strafe_Y
+        self.pwm_signals.pwm3 = control.turn_X
+        self.pwm_signals.pwm4 = control.turn_Y
+        self.pwm_signals.pwm5 = control.ascend
+        self.pwm_signals.pwm6 = control.descend
+
+        self.pwm_publisher.publish(self.pwm_signals)
 
 if __name__ == '__main__': 
-    rospy.init_node('mux_node', anonymous=True)
-    rate = rospy.Rate(10)
-    rospy.Subscriber('control', control, controller_callback)
-    rospy.spin()
-    
+    try:
+        hmi_node = HmiNode()
+        hmi_node.start_node()
+    except rospy.ROSInterruptException:
+        pass
+
+
+  

c77c7450bcb86233c6d15fb419e7975ef8df4471
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-02-25 19:49:56 +0100

    Update with new state and local variables

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,9 +1,8 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Wrench.h"
 #include "uranus_dp/State.h"
-
-// NOTE: For now, I juse use Pose messages for state and setpoints, which is probably insufficient.
 
 class Controller
 {
@@ -12,31 +11,53 @@ public:
     {
         outputPub = n.advertise<geometry_msgs::Wrench>("outputTopic", 1);
 
-        // I don't know why, but "this" is necessary here
-        stateSub = n.subscribe("state", 1, &Controller::stateCallback, this);
-        setpointSub = n.subscribe("setpoint", 1, &Controller::setpointCallback, this);
+        // I don't know why, but 'this' is necessary here
+        stateSub = n.subscribe("stateTopic", 1, &Controller::stateCallback, this);
+        setpointSub = n.subscribe("setpointTopic", 1, &Controller::setpointCallback, this);
     }
 
-    // stateCallback updates the private state variable when a new state message arrives.
-    void stateCallback(const geometry_msgs::Pose &state_msg)
+    // stateCallback updates the private state variables when a new state message arrives.
+    void stateCallback(const uranus_dp::State &state_msg)
     {
-        state = state_msg;
+        // Copy pose values (position and orientation)
+        eta[0] = state_msg.pose.position.x;
+        eta[1] = state_msg.pose.position.y;
+        eta[2] = state_msg.pose.position.z;
+        eta[3] = state_msg.pose.orientation.x;
+        eta[4] = state_msg.pose.orientation.y;
+        eta[5] = state_msg.pose.orientation.z;
+        eta[6] = state_msg.pose.orientation.w;
+
+        // Copy twist values (linear and angular velocity)
+        nu[0] = state_msg.twist.linear.x;
+        nu[1] = state_msg.twist.linear.y;
+        nu[2] = state_msg.twist.linear.z;
+        nu[3] = state_msg.twist.angular.x;
+        nu[4] = state_msg.twist.angular.y;
+        nu[5] = state_msg.twist.angular.z;
     }
 
     // setpointCallback updates the private setpoint variable when a new setpoint message arrives.
-    void setpointCallback(const geometry_msgs::Pose &setpoint_msg)
+    void setpointCallback(const geometry_msgs::Twist &setpoint_msg)
     {
-        setpoint = setpoint_msg;
+        // Copy velocity setpoint values
+        nu_sp[0] = setpoint_msg.linear.x;
+        nu_sp[1] = setpoint_msg.linear.y;
+        nu_sp[2] = setpoint_msg.linear.z;
+        nu_sp[3] = setpoint_msg.angular.x;
+        nu_sp[4] = setpoint_msg.angular.y;
+        nu_sp[5] = setpoint_msg.angular.z;
     }
 
     // calculate contains the control algorithm, and calculates the control output based on the
     // current state and setpoint.
     void calculate(void)
     {
+        // Only velocity control for now.
+
+        // geometry_msgs::Twist velocityError = setpoint - state.velocity;
+
         geometry_msgs::Wrench output;
-
-        // Put control algorithm here.
-
         output.force.x = 0;
         output.force.y = 0;
         output.force.z = 0;
@@ -48,11 +69,17 @@ public:
     }
 private:
     ros::NodeHandle n;
+
     ros::Publisher  outputPub;
     ros::Subscriber stateSub;
     ros::Subscriber setpointSub;
-    geometry_msgs::Pose state;
-    geometry_msgs::Pose setpoint;
+
+    // State as pose eta = [p q], and twist nu = [v omega]
+    double eta [7];
+    double nu [6];
+
+    // Velocity setpoints nu_sp = [v_sp omega_sp]
+    double nu_sp [6];
 }; // End of class Controller
 
 int main(int argc, char **argv)

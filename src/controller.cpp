ff843f81720a5c502aa764fabf673130fe4d9e9f
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-02-26 10:36:22 +0100

    Added more necessary variables and some math

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,6 +3,8 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Wrench.h"
 #include "uranus_dp/State.h"
+#include <Eigen/Dense>
+#include <cmath>
 
 class Controller
 {
@@ -14,59 +16,116 @@ public:
         // I don't know why, but 'this' is necessary here
         stateSub = n.subscribe("stateTopic", 1, &Controller::stateCallback, this);
         setpointSub = n.subscribe("setpointTopic", 1, &Controller::setpointCallback, this);
+
+        // Allocate dynamic member variables
+        T = Eigen::MatrixXd(4,3);
+        tau = Eigen::VectorXd(3);
+
+        // Initialize controller gains
+        K_p = Eigen::Matrix3d::Identity();
+        K_d = Eigen::Matrix3d::Identity();
     }
 
     // stateCallback updates the private state variables when a new state message arrives.
     void stateCallback(const uranus_dp::State &state_msg)
     {
-        // Copy pose values (position and orientation)
-        eta[0] = state_msg.pose.position.x;
-        eta[1] = state_msg.pose.position.y;
-        eta[2] = state_msg.pose.position.z;
-        eta[3] = state_msg.pose.orientation.x;
-        eta[4] = state_msg.pose.orientation.y;
-        eta[5] = state_msg.pose.orientation.z;
-        eta[6] = state_msg.pose.orientation.w;
+        // Copy position values
+        p[0] = state_msg.pose.position.x;
+        p[1] = state_msg.pose.position.y;
+        p[2] = state_msg.pose.position.z;
 
-        // Copy twist values (linear and angular velocity)
-        nu[0] = state_msg.twist.linear.x;
-        nu[1] = state_msg.twist.linear.y;
-        nu[2] = state_msg.twist.linear.z;
-        nu[3] = state_msg.twist.angular.x;
-        nu[4] = state_msg.twist.angular.y;
-        nu[5] = state_msg.twist.angular.z;
+        // Copy orientation values (quaternion)
+        q.w()      = state_msg.pose.orientation.x;
+        q.x() = state_msg.pose.orientation.y;
+        q.y() = state_msg.pose.orientation.z;
+        q.z() = state_msg.pose.orientation.w;
+
+        // Copy linear velocity values
+        v[0] = state_msg.twist.linear.x;
+        v[1] = state_msg.twist.linear.y;
+        v[2] = state_msg.twist.linear.z;
+
+        // Copy angular velocity values
+        omega[0] = state_msg.twist.angular.x;
+        omega[1] = state_msg.twist.angular.y;
+        omega[2] = state_msg.twist.angular.z;
     }
 
     // setpointCallback updates the private setpoint variable when a new setpoint message arrives.
     void setpointCallback(const geometry_msgs::Twist &setpoint_msg)
     {
-        // Copy velocity setpoint values
-        nu_sp[0] = setpoint_msg.linear.x;
-        nu_sp[1] = setpoint_msg.linear.y;
-        nu_sp[2] = setpoint_msg.linear.z;
-        nu_sp[3] = setpoint_msg.angular.x;
-        nu_sp[4] = setpoint_msg.angular.y;
-        nu_sp[5] = setpoint_msg.angular.z;
+        // Copy linear velocity setpoint values
+        v_sp[0] = setpoint_msg.linear.x;
+        v_sp[1] = setpoint_msg.linear.y;
+        v_sp[2] = setpoint_msg.linear.z;
+
+        // Copy angular velocity setpoint values
+        omega_sp[0] = setpoint_msg.angular.x;
+        omega_sp[1] = setpoint_msg.angular.y;
+        omega_sp[2] = setpoint_msg.angular.z;
     }
 
     // calculate contains the control algorithm, and calculates the control output based on the
     // current state and setpoint.
     void calculate(void)
     {
-        // Only velocity control for now.
+        // Only orientation control for now.
 
-        // geometry_msgs::Twist velocityError = setpoint - state.velocity;
+        // Orientation error
+        // q_err.x() = q_sp.x() - q.x();
 
+        updateTransformationMatrices();
+
+        // Control output
+        // tau = - K_d * omega - K_p * T.transpose() * q_err;
+        // tau = K_p * T.transpose() * q_err;
+        // Eigen::Vector3d temp = T.transpose() * q_err;
+
+        // Build output message
         geometry_msgs::Wrench output;
         output.force.x = 0;
         output.force.y = 0;
         output.force.z = 0;
-        output.torque.x = 0;
-        output.torque.y = 0;
-        output.torque.z = 0;
-
+        output.torque.x = tau(0);
+        output.torque.y = tau(1);
+        output.torque.z = tau(2);
         outputPub.publish(output);
     }
+
+    void updateTransformationMatrices(void)
+    {
+        // Linear velocity transformation matrix
+        R(0,0) = 1 - 2*(pow(q.y(),2) + pow(q.z(),2));
+        R(0,1) = 2*(q.x()*q.y() - q.z()*q.w());
+        R(0,2) = 2*(q.x()*q.z() - q.y()*q.w());
+        
+        R(1,0) = 2*(q.x()*q.y() + q.z()*q.w());
+        R(1,1) = 1 - 2*(pow(q.x(),2) + pow(q.w(),2));
+        R(1,2) = 2*(q.y()*q.z() + q.x()*q.w());
+        
+        R(2,0) = 2*(q.x()*q.z() + q.y()*q.w());
+        R(2,1) = 2*(q.y()*q.z() + q.x()*q.w());
+        R(2,2) = 1 - 2*(pow(q.x(),2) + pow(q.y(),2));
+
+        // Angular velocity transformation matrix
+        T(0,0) = -q.x();
+        T(0,1) = -q.y();
+        T(0,2) = -q.z();
+
+        T(1,0) =  q.w();
+        T(1,1) = -q.z();
+        T(1,2) =  q.y();
+
+        T(2,0) =  q.z();
+        T(2,1) =  q.z();
+        T(2,2) = -q.x();
+
+        T(3,0) = -q.y();
+        T(3,1) =  q.x();
+        T(3,2) =  q.w();
+
+        T *= 0.5;
+    } 
 private:
     ros::NodeHandle n;
 
@@ -74,12 +133,34 @@ private:
     ros::Subscriber stateSub;
     ros::Subscriber setpointSub;
 
-    // State as pose eta = [p q], and twist nu = [v omega]
-    double eta [7];
-    double nu [6];
+    // State
+    Eigen::Vector3d p;     // Position
+    Eigen::Quaterniond q;  // Orientation
+    Eigen::Vector3d v;     // Linear velocity
+    Eigen::Vector3d omega; // Angular velocity
 
-    // Velocity setpoints nu_sp = [v_sp omega_sp]
-    double nu_sp [6];
+    // Setpoints
+    Eigen::Vector3d p_sp;     // Position
+    Eigen::Quaterniond q_sp;  // Orientation
+    Eigen::Vector3d v_sp;     // Linear velocity
+    Eigen::Vector3d omega_sp; // Angular velocity
+
+    // Errors
+    Eigen::Vector3d p_err;     // Position
+    Eigen::Quaterniond q_err;  // Orientation
+    Eigen::Vector3d v_err;     // Linear velocity
+    Eigen::Vector3d omega_err; // Angular velocity
+
+    // Transformation matrices
+    Eigen::Matrix3d R;
+    Eigen::MatrixXd T;
+
+    // Control output
+    Eigen::VectorXd tau;
+
+    // Controller gains
+    Eigen::Matrix3d K_d;
+    Eigen::Matrix3d K_p;
 }; // End of class Controller
 
 int main(int argc, char **argv)

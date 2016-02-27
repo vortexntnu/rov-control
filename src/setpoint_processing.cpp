899a93c60d702520643da79ff86a69df2c928c9f
peteraaser <peteraaser@gmail.com>
2016-02-27 17:10:43 +0100

    Add code for processing movement joystick input

--- src/setpoint_processing.cpp
+++ src/setpoint_processing.cpp
@@ -4,30 +4,46 @@
 #include <iostream>
 #include <string.h>
 #include "geometry_msgs/Twist.h"
+#include <joystick/directional_input.h>
 
+// Delivers twist.msg
 
 class SetpointProcessing{
 public:
     SetpointProcessing(){
-        pub = n.advertise<std_msgs::String>("s1", 1);
+        pub = n.advertise<geometry_msgs::Twist>("temp", 1);
         sub = n.subscribe("joy_input", 1, &SetpointProcessing::callback, this);
     }
 
-    void callback(const uranus_dp::JoystickUranus& input)
+    void callback(const joystick::directional_input& input)
     {
         geometry_msgs::Twist output;
-        output.linear.x = input.surge;
-        output.linear.y = input.sway;
-        output.linear.z = input.heave;
-        output.angular.x = input.roll;
-        output.angular.y = input.pitch;
-        output.angular.z = input.yaw;
+
+        output.linear.x = (input.strafe_X/STRAFE_RANGE)*MAX_LINEAR_X;
+        output.linear.y = (input.strafe_Y/STRAFE_RANGE)*MAX_LINEAR_Y;
+        output.linear.z = (input.ascend/ASCEND_RANGE)*MAX_LINEAR_Z;
+        output.angular.x = 0.0;
+        output.angular.y = (input.turn_Y/TURN_RANGE)*MAX_ANGULAR_X;
+        output.angular.z = (input.turn_X/TURN_RANGE)*MAX_ANGULAR_Z;
+
+        pub.publish(output);
     }
 
 private:
     ros::NodeHandle n;
     ros::Publisher  pub;
     ros::Subscriber sub;
+
+    static const double MAX_LINEAR_X  = 1.0;
+    static const double MAX_LINEAR_Y  = 1.0;
+    static const double MAX_LINEAR_Z  = 1.0;
+    static const double MAX_ANGULAR_X = 1.0;
+    static const double MAX_ANGULAR_Y = 1.0;
+    static const double MAX_ANGULAR_Z = 1.0;
+
+    static const int STRAFE_RANGE = (2 << 16);
+    static const int TURN_RANGE   = (2 << 16);
+    static const int ASCEND_RANGE = (2 << 16);
 };
 
 int main(int argc, char** argv){

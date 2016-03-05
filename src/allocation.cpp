d8258dad7e36b303aae20af930796157e539404c
efos <eiriklothefoseid@gmail.com>
2016-03-05 17:09:08 +0000

    Fixed the allocator node
    Made the allocator into its own class since this made the most sense.
    Uses the equations from Fossen 2011 chapter 12.3.2 to
    calculate the six thrusterforces (published on topic "U" as a uranus_dp::thrusterforces)
    from the wanted forces and torques from the controller node.

--- src/allocation.cpp
+++ src/allocation.cpp
@@ -13,38 +13,63 @@
 #include "ros/ros.h"
 #include <iostream>
 #include <Eigen/Dense> // Library for matrix operations
+#include "geometry_msgs/Wrench.h"
+#include "uranus_dp/ThrusterForces.h"
 
-// using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+
 const int nActuators = 6; // Move to parameter server
 
+class Allocator{
+    private:
+        ros::NodeHandle n;
+        ros::Subscriber sub;
+        ros::Publisher pub;
+        MatrixXd T_pseudoinverse;
+        VectorXd uVector;
+        uranus_dp::ThrusterForces u;
+        VectorXd tau;
+        MatrixXd K;
+        MatrixXd T;
+    public:
+        Allocator(){
+            sub = n.subscribe("forces", 1, &Allocator::tauCallBack, this);
+            pub = n.advertise<uranus_dp::ThrusterForces>("U", 1);
+            tau = VectorXd(6);
+        }
+        void tauCallBack(const geometry_msgs::Wrench& tauMsg){
+            double thrustCoeff = 5;
+            K = (thrustCoeff * VectorXd::Ones(nActuators)).asDiagonal();
+            T = MatrixXd::Random(6,6);
+
+            tau(0) = tauMsg.force.x;
+            tau(1) = tauMsg.force.y;
+            tau(2) = tauMsg.force.z;
+            tau(3) = tauMsg.torque.x;
+            tau(4) = tauMsg.torque.y;
+            tau(5) = tauMsg.torque.z;
+
+            T_pseudoinverse = T.transpose() * (T * T.transpose()).inverse();
+            uVector = K.inverse() * T_pseudoinverse * tau;
+
+            u.F1 = uVector(0);
+            u.F2 = uVector(1);
+            u.F3 = uVector(2);
+            u.F4 = uVector(3);
+            u.F5 = uVector(4);
+            u.F6 = uVector(5);
+
+
+            pub.publish(u);
+        }
+
+};
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "allocation");
-    ros::NodeHandle n;
-    // ros::Subscriber sub = n.subscribe("forces", 1 );
-
-    // K and T should probably be given by a ROS parameter server.
-    // For now, temporary values are hard-coded here.
-    double thrustCoeff = 5; // Use same value for all actuators as they are the same.
-    MatrixXd K = (thrustCoeff * VectorXd::Ones(nActuators)).asDiagonal();
-    MatrixXd T = MatrixXd::Random(6,6);
-
-    //
-    // Place inside a callback function:
-    //
-
-    // read tau from topic (or just hardcode one for now)
-    VectorXd tau(6);
-    tau << 1,2,3,4,5,6;
-
-    // then calculate the control inputs with Moore-Penrose pseudoinverse
-    MatrixXd T_pseudoinverse = T.transpose() * (T * T.transpose()).inverse();
-    VectorXd u = K.inverse() * T_pseudoinverse * tau;
-
-    // send u on some topic
-
+    Allocator allocator;
+    ros::spin();
     return 0;
 }

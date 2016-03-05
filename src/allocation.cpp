b802fd680d19f87aca2779410f7e0c98128e799a
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-03-05 21:56:42 +0100

    Minor fixes to allocator
    Mostly just moving things around to match other nodes and renaming stuff

--- src/allocation.cpp
+++ src/allocation.cpp
@@ -12,60 +12,60 @@
 
 #include "ros/ros.h"
 #include <iostream>
-#include <Eigen/Dense> // Library for matrix operations
+#include <Eigen/Dense>
 #include "geometry_msgs/Wrench.h"
 #include "uranus_dp/ThrusterForces.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+const int nThrusters = 6; // Move to parameter server
+const double thrustCoeff = 5; // Ditto
 
-const int nActuators = 6; // Move to parameter server
+class Allocator
+{
+public:
+    Allocator()
+    {
+        tauSub = n.subscribe("forces", 1, &Allocator::tauCallBack, this);
+        uPub   = n.advertise<uranus_dp::ThrusterForces>("U", 1);
+        tau = VectorXd(6);
+    }
 
-class Allocator{
-    private:
-        ros::NodeHandle n;
-        ros::Subscriber sub;
-        ros::Publisher pub;
-        MatrixXd T_pseudoinverse;
-        VectorXd uVector;
-        uranus_dp::ThrusterForces u;
-        VectorXd tau;
-        MatrixXd K;
-        MatrixXd T;
-    public:
-        Allocator(){
-            sub = n.subscribe("forces", 1, &Allocator::tauCallBack, this);
-            pub = n.advertise<uranus_dp::ThrusterForces>("U", 1);
-            tau = VectorXd(6);
-        }
-        void tauCallBack(const geometry_msgs::Wrench& tauMsg){
-            double thrustCoeff = 5;
-            K = (thrustCoeff * VectorXd::Ones(nActuators)).asDiagonal();
-            T = MatrixXd::Random(6,6);
+    void tauCallBack(const geometry_msgs::Wrench& tauMsg){
+        tau(0) = tauMsg.force.x;
+        tau(1) = tauMsg.force.y;
+        tau(2) = tauMsg.force.z;
+        tau(3) = tauMsg.torque.x;
+        tau(4) = tauMsg.torque.y;
+        tau(5) = tauMsg.torque.z;
 
-            tau(0) = tauMsg.force.x;
-            tau(1) = tauMsg.force.y;
-            tau(2) = tauMsg.force.z;
-            tau(3) = tauMsg.torque.x;
-            tau(4) = tauMsg.torque.y;
-            tau(5) = tauMsg.torque.z;
+        K = thrustCoeff * MatrixXd::Identity(nThrusters, nThrusters);
+        T = MatrixXd::Random(6,6);
+        T_pseudoinverse = T.transpose() * (T * T.transpose()).inverse();
+        u = K.inverse() * T_pseudoinverse * tau;
 
-            T_pseudoinverse = T.transpose() * (T * T.transpose()).inverse();
-            uVector = K.inverse() * T_pseudoinverse * tau;
+        uranus_dp::ThrusterForces uMsg;
+        uMsg.F1 = u(0);
+        uMsg.F2 = u(1);
+        uMsg.F3 = u(2);
+        uMsg.F4 = u(3);
+        uMsg.F5 = u(4);
+        uMsg.F6 = u(5);
+        uPub.publish(uMsg);
+    }
+private:
+    ros::NodeHandle n;
+    ros::Subscriber tauSub;
+    ros::Publisher uPub;
 
-            u.F1 = uVector(0);
-            u.F2 = uVector(1);
-            u.F3 = uVector(2);
-            u.F4 = uVector(3);
-            u.F5 = uVector(4);
-            u.F6 = uVector(5);
+    VectorXd u;
+    VectorXd tau;
+    MatrixXd K;
+    MatrixXd T;
+    MatrixXd T_pseudoinverse;
+}; // End of class Allocator
 
-
-            pub.publish(u);
-        }
-
-};
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "allocation");

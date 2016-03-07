0afe96c76840c7a5338eaa5976ca6b3e04ab146b
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-03-07 20:46:56 +0100

    Write like half of a kalman filter

---
+++ src/extended_kalman_filter.cpp
@@ -0,0 +1,82 @@
+// See
+// Fossen 2011, chapter 11.3.3
+// and
+// Dukan 2014, chapter 4.2.2
+
+#include "extended_kalman_filter.h"
+
+ExtendedKalmanFilter::ExtendedKalmanFilter(double sampleTime)
+{
+    h = sampleTime;
+
+    Q.setIdentity();
+    R.setIdentity();
+    Gamma = h*E;
+}
+
+void ExtendedKalmanFilter::controlCallback(const geometry_msgs::Wrench &tauMsg)
+{
+    // The input to the model used for filter design is tau (control forces), not u (thruster forces),
+    // but in the context of the filter, it makes sense to use the name u. Mmmmmkay?
+    u << tauMsg.force.x,
+         tauMsg.force.y,
+         tauMsg.force.z,
+         tauMsg.torque.x,
+         tauMsg.torque.y,
+         tauMsg.torque.z;
+}
+
+void ExtendedKalmanFilter::update()
+{
+    // Correct
+    K = P_bar*H_transpose * (H*P_bar*H_transpose + R).inverse();
+    x_hat = x_bar + K*(y - H*x_bar);
+    P_hat = (I - K*H)*P_bar*(I - K*H).transpose() + K*R*K.transpose();
+
+    // Update f and Phi
+    updateSystemDynamics();
+    updatePhi();
+
+    // Predict
+    x_bar = x_hat + h*(f + B*u);
+    P_bar = Phi*P_hat*Phi.transpose() + Gamma*Q*Gamma.transpose();
+
+    // Publish
+    uranus_dp::State xMsg;
+    xMsg.pose.position.x    = x_bar(0);
+    xMsg.pose.position.y    = x_bar(1);
+    xMsg.pose.position.z    = x_bar(2);
+    xMsg.pose.orientation.x = x_bar(3);
+    xMsg.pose.orientation.y = x_bar(4);
+    xMsg.pose.orientation.z = x_bar(5);
+    xMsg.pose.orientation.w = x_bar(6);
+    xMsg.twist.linear.x     = x_bar(7);
+    xMsg.twist.linear.y     = x_bar(8);
+    xMsg.twist.linear.z     = x_bar(9);
+    xMsg.twist.angular.x    = x_bar(10);
+    xMsg.twist.angular.y    = x_bar(11);
+    xMsg.twist.angular.z    = x_bar(12);
+    statePub.publish(xMsg);
+}
+
+void ExtendedKalmanFilter::updateSystemDynamics()
+{
+    // These need to actually get some values later:
+    Eigen::Matrix<double,7,6> J;
+    Eigen::Matrix<double,7,1> eta;
+    Eigen::Matrix<double,6,1> nu;
+    Eigen::Matrix<double,6,6> M;
+    Eigen::Matrix<double,6,6> C;
+    Eigen::Matrix<double,6,6> D;
+    Eigen::Matrix<double,6,1> g;
+
+    f << J*nu,
+         -M.inverse() * (C*nu + D*nu + g);
+}
+
+void ExtendedKalmanFilter::updatePhi()
+{
+    Eigen::Matrix<double,13,13> del_f_del_x;
+    // Evalute del_f_del_x at current x_hat
+    Phi = I + h*del_f_del_x;
+}
---
+++ src/extended_kalman_filter.h
@@ -0,0 +1,55 @@
+#ifndef EXTENDED_KALMAN_FILTER_H
+#define EXTENDED_KALMAN_FILTER_H
+
+#include "ros/ros.h"
+#include <Eigen/Dense>
+#include "uranus_dp/State.h"
+#include "geometry_msgs/Wrench.h"
+
+class ExtendedKalmanFilter
+{
+public:
+    ExtendedKalmanFilter(double sampleTime);
+    void controlCallback(const geometry_msgs::Wrench &uMsg);
+    // void sensorCallback(const uranus_dp::Sensor &yMsg);
+    void update();
+private:
+    ros::NodeHandle nh;
+    ros::Subscriber controlSub;
+    ros::Subscriber sensorSub;
+    ros::Publisher  statePub;
+
+    // Sampling time
+    double h;
+
+    // System variables
+    Eigen::Matrix<double,13, 1> x; // State
+    Eigen::Matrix<double,13, 1> f; // System dynamics
+    Eigen::Matrix<double,13, 6> B; // Input dynamics
+    Eigen::Matrix<double, 6, 1> u; // Input
+    Eigen::Matrix<double,13,13> E; // System noise dynamics
+    Eigen::Matrix<double,13, 1> w; // System noise
+    Eigen::Matrix<double,10, 1> y; // Measurements
+    Eigen::Matrix<double,10,13> H; // Sensor dynamics
+    Eigen::Matrix<double,10, 1> v; // Sensor noise
+
+    // Filter variables
+    Eigen::Matrix<double,13,13> Q;     // Design matrix
+    Eigen::Matrix<double,10,10> R;     // Design matrix
+    Eigen::Matrix<double,13,10> K;     // Kalman gain
+    Eigen::Matrix<double,13, 1> x_hat; // State estimate
+    Eigen::Matrix<double,13, 1> x_bar; // State projection (or something)
+    Eigen::Matrix<double,13,13> P_hat; // Covariance estimate
+    Eigen::Matrix<double,13,13> P_bar; // Covariance projection (or something)
+    Eigen::Matrix<double,13,13> Phi;   // 
+    Eigen::Matrix<double,13,13> Gamma; // 
+
+    // Helpful extra shit
+    Eigen::Matrix<double,13,13> I;     // 13*13 identity
+    Eigen::Matrix<double,13,10> H_transpose;
+
+    void updateSystemDynamics();
+    void updatePhi();
+};
+
+#endif

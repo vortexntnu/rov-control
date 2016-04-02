3ff621091a8f36f761da629d43bdbf2581b5fd83
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-02 16:48:42 +0200

    Add launch info message

--- src/allocation.cpp
+++ src/allocation.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "allocation");
+    ROS_INFO("Launching node allocation.\n");
     LagrangeAllocator allocator;
     allocator.setWeights(Eigen::MatrixXd::Identity(6,6));
     ros::spin();

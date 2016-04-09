2ec4f828e595de36e2b920066d51befeb934f5d4
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-10 01:12:07 +0200

    Remove unnecessary scaling

--- src/test/allocator_test.cpp
+++ src/test/allocator_test.cpp
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 #include "geometry_msgs/Wrench.h"
 #include "uranus_dp/ThrusterForces.h"
+#include <eigen_conversions/eigen_msg.h>
 
 class AllocatorTest : public ::testing::Test {
 public:
@@ -24,12 +25,12 @@ public:
     void Publish(double surge, double sway, double heave, double roll, double pitch, double yaw)
     {
         geometry_msgs::Wrench msg;
-        msg.force.x = surge  * NORMALIZATION * SCALING_LIN;
-        msg.force.y = sway   * NORMALIZATION * SCALING_LIN;
-        msg.force.z = heave  * NORMALIZATION * SCALING_LIN;
-        msg.torque.x = roll  * NORMALIZATION * SCALING_ANG;
-        msg.torque.y = pitch * NORMALIZATION * SCALING_ANG;
-        msg.torque.z = yaw   * NORMALIZATION * SCALING_ANG;
+        msg.force.x  = surge;
+        msg.force.y  = sway;
+        msg.force.z  = heave;
+        msg.torque.x = roll;
+        msg.torque.y = pitch;
+        msg.torque.z = yaw;
         publisher_.publish(msg);
     }
 
@@ -49,10 +50,6 @@ public:
     ros::Publisher  publisher_;
     ros::Subscriber subscriber_;
     bool message_received_;
-
-    static const double NORMALIZATION  = 0.000030517578125; // Scale inputs down to [-1, 1]
-    static const double SCALING_LIN    = 10;                // [N]  Max force in given direction
-    static const double SCALING_ANG    = 2;                 // [Nm] Max torque around given axis
 
     void Callback(const uranus_dp::ThrusterForces& msg)
     {
@@ -74,27 +71,29 @@ public:
 
 TEST_F(AllocatorTest, CheckResponsiveness)
 {
-    Publish(1, 2, 3, 4, 5, 5);
+    Publish(1,2,3,4,5,6);
     WaitForMessage();
-    ASSERT_TRUE(true);
+    EXPECT_TRUE(true);
 }
 
 TEST_F(AllocatorTest, NormalInput)
 {
-    Publish(-29051, -27049, 0, 0, 24354, -7259);
+    Publish(3.286, -7.510, 0, 0, -1.272, 0.196);
     WaitForMessage();
-    EXPECT_NEAR(u_(0),  2.237187310000600, EPSILON);
-    EXPECT_NEAR(u_(1), -3.599822806695235, EPSILON);
-    EXPECT_NEAR(u_(2),  2.729657728596679, EPSILON);
-    EXPECT_NEAR(u_(3), -3.107352388099156, EPSILON);
-    EXPECT_NEAR(u_(4), -3.720152593089502, EPSILON);
-    EXPECT_NEAR(u_(5), -3.914921574557270, EPSILON);
+
+    EXPECT_NEAR(u_(0),  2.089572758999718, EPSILON);
+    EXPECT_NEAR(u_(1), -3.220850094910619, EPSILON);
+    EXPECT_NEAR(u_(2),  1.871711824118534, EPSILON);
+    EXPECT_NEAR(u_(3), -3.438711029791804, EPSILON);
+    EXPECT_NEAR(u_(4),  2.553894350261510, EPSILON);
+    EXPECT_NEAR(u_(5),  2.640056992292659, EPSILON);
 }
 
 TEST_F(AllocatorTest, MinimumInput)
 {
-    Publish(-32768, -32768, 0, 0, -32768, -32768);
+    Publish(-10, -10, 0, 0, -2, -2);
     WaitForMessage();
+
     EXPECT_NEAR(u_(0), -2.328576868278356, EPSILON);
     EXPECT_NEAR(u_(1), -9.399712492659631, EPSILON);
     EXPECT_NEAR(u_(2), -0.105506104184636, EPSILON);
@@ -105,20 +104,22 @@ TEST_F(AllocatorTest, MinimumInput)
 
 TEST_F(AllocatorTest, MaximumInput)
 {
-    Publish(32767, 32767, 0, 0, 32767, 32767);
+    Publish(10, 10, 0, 0, 2, 2);
     WaitForMessage();
-    EXPECT_NEAR(u_(0),  2.328505805751858, EPSILON);
-    EXPECT_NEAR(u_(1),  9.399425636199283, EPSILON);
-    EXPECT_NEAR(u_(2),  0.105502884393859, EPSILON);
-    EXPECT_NEAR(u_(3),  7.176422714841284, EPSILON);
-    EXPECT_NEAR(u_(4), -2.160679445597902, EPSILON);
-    EXPECT_NEAR(u_(5), -1.281495644006171, EPSILON);
+
+    EXPECT_NEAR(u_(0),  2.328576868278356, EPSILON);
+    EXPECT_NEAR(u_(1),  9.399712492659631, EPSILON);
+    EXPECT_NEAR(u_(2),  0.105506104184636, EPSILON);
+    EXPECT_NEAR(u_(3),  7.17664172856591, EPSILON);
+    EXPECT_NEAR(u_(4), -2.160745386314036, EPSILON);
+    EXPECT_NEAR(u_(5), -1.281534753343126, EPSILON);
 }
 
 TEST_F(AllocatorTest, ZeroInput)
 {
     Publish(0, 0, 0, 0, 0, 0);
     WaitForMessage();
+
     EXPECT_NEAR(u_(0), 0, EPSILON);
     EXPECT_NEAR(u_(1), 0, EPSILON);
     EXPECT_NEAR(u_(2), 0, EPSILON);

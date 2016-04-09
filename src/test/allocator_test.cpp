24c2927108247e9b49430e494c97c444c4765651
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-09 15:20:39 +0200

    Rename and finish allocator test

--- src/test/allocator_test.cpp
+++ src/test/allocator_test.cpp
@@ -4,12 +4,12 @@
 #include "geometry_msgs/Wrench.h"
 #include "uranus_dp/ThrusterForces.h"
 
-class LagrangeAllocatorTest : public ::testing::Test {
+class AllocatorTest : public ::testing::Test {
 public:
-    LagrangeAllocatorTest()
+    AllocatorTest()
     {
         publisher_  = node_handle_.advertise<geometry_msgs::Wrench>("control_forces", 5);
-        subscriber_ = node_handle_.subscribe("control_inputs", 5, &LagrangeAllocatorTest::Callback, this);
+        subscriber_ = node_handle_.subscribe("control_inputs", 5, &AllocatorTest::Callback, this);
         message_received_ = false;
     }
 
@@ -42,7 +42,7 @@ public:
     }
 
     Eigen::Matrix<double,6,1> u_;
-    static const double EPSILON = 0.000000000001;
+    static const double EPSILON = 1e-6; // Max absolute error 1 micronewton
 
  private:
     ros::NodeHandle node_handle_;
@@ -72,23 +72,66 @@ public:
     }
 };
 
-TEST_F(LagrangeAllocatorTest, NormalInput)
+TEST_F(AllocatorTest, CheckResponsiveness)
+{
+    Publish(1, 2, 3, 4, 5, 5);
+    WaitForMessage();
+    ASSERT_TRUE(true);
+}
+
+TEST_F(AllocatorTest, NormalInput)
 {
     Publish(-29051, -27049, 0, 0, 24354, -7259);
     WaitForMessage();
-    EXPECT_TRUE(abs(u_(0) - ( 2.237364853185521)) < EPSILON);
-    EXPECT_TRUE(abs(u_(1) - (-3.600108488633174)) < EPSILON);
-    EXPECT_TRUE(abs(u_(2) - ( 2.729874354234020)) < EPSILON);
-    EXPECT_TRUE(abs(u_(3) - (-3.107598987584675)) < EPSILON);
-    EXPECT_TRUE(abs(u_(4) - (-3.720447824399290)) < EPSILON);
-    EXPECT_TRUE(abs(u_(5) - (-3.915232262733428)) < EPSILON);
+    EXPECT_NEAR(u_(0),  2.237187310000600, EPSILON);
+    EXPECT_NEAR(u_(1), -3.599822806695235, EPSILON);
+    EXPECT_NEAR(u_(2),  2.729657728596679, EPSILON);
+    EXPECT_NEAR(u_(3), -3.107352388099156, EPSILON);
+    EXPECT_NEAR(u_(4), -3.720152593089502, EPSILON);
+    EXPECT_NEAR(u_(5), -3.914921574557270, EPSILON);
+}
+
+TEST_F(AllocatorTest, MinimumInput)
+{
+    Publish(-32768, -32768, 0, 0, -32768, -32768);
+    WaitForMessage();
+    EXPECT_NEAR(u_(0), -2.328576868278356, EPSILON);
+    EXPECT_NEAR(u_(1), -9.399712492659631, EPSILON);
+    EXPECT_NEAR(u_(2), -0.105506104184636, EPSILON);
+    EXPECT_NEAR(u_(3), -7.176641728565912, EPSILON);
+    EXPECT_NEAR(u_(4),  2.160745386314036, EPSILON);
+    EXPECT_NEAR(u_(5),  1.281534753343126, EPSILON);
+}
+
+TEST_F(AllocatorTest, MaximumInput)
+{
+    Publish(32767, 32767, 0, 0, 32767, 32767);
+    WaitForMessage();
+    EXPECT_NEAR(u_(0),  2.328505805751858, EPSILON);
+    EXPECT_NEAR(u_(1),  9.399425636199283, EPSILON);
+    EXPECT_NEAR(u_(2),  0.105502884393859, EPSILON);
+    EXPECT_NEAR(u_(3),  7.176422714841284, EPSILON);
+    EXPECT_NEAR(u_(4), -2.160679445597902, EPSILON);
+    EXPECT_NEAR(u_(5), -1.281495644006171, EPSILON);
+}
+
+TEST_F(AllocatorTest, ZeroInput)
+{
+    Publish(0, 0, 0, 0, 0, 0);
+    WaitForMessage();
+    EXPECT_NEAR(u_(0), 0, EPSILON);
+    EXPECT_NEAR(u_(1), 0, EPSILON);
+    EXPECT_NEAR(u_(2), 0, EPSILON);
+    EXPECT_NEAR(u_(3), 0, EPSILON);
+    EXPECT_NEAR(u_(4), 0, EPSILON);
+    EXPECT_NEAR(u_(5), 0, EPSILON);
 }
 
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
-    ros::init(argc, argv, "lagrange_allocator_test");
-    ROS_INFO("Launching node lagrange_allocator_test.");
+    ros::init(argc, argv, "allocator_test");
+    ROS_INFO("Launching node allocator_test.");
 
     int ret = RUN_ALL_TESTS();
     ros::shutdown();

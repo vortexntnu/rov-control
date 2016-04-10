7794fd4fc6d32efd3e83a2acf701f7c5b3e45c5e
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-10 23:12:27 +0200

    Outsource some custom Eigen typedefs

---
+++ src/eigen_typedefs.h
@@ -0,0 +1,12 @@
+#ifndef EIGEN_TYPEDEFS_H
+#define EIGEN_TYPEDEFS_H
+
+#include <Eigen/Dense>
+
+namespace Eigen
+{
+typedef Eigen::Matrix<double,6,6> Matrix6d;
+typedef Eigen::Matrix<double,6,1> Vector6d;
+}
+
+#endif

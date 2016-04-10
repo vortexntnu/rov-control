5d81fbb6d68a672e26b34c176ba32ad178cd6e62
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-10 14:11:44 +0200

    Use new SetControlMode

--- src/setpoint_processing.cpp
+++ src/setpoint_processing.cpp
@@ -3,7 +3,8 @@
 #include "geometry_msgs/Pose.h"
 #include <joystick/DirectionalInput.h>
 #include <string>
-#include "uranus_dp/ToggleControlMode.h"
+#include "uranus_dp/SetControlMode.h"
+#include "control_mode_enum.h"
 
 class SetpointProcessing
 {
@@ -13,24 +14,25 @@ public:
         joystickSub = nh.subscribe("joy_input", 1, &SetpointProcessing::callback, this);
         wrenchPub   = nh.advertise<geometry_msgs::Wrench>("open_loop_setpoint", 1);
         posePub     = nh.advertise<geometry_msgs::Pose>("stationkeeping_setpoint", 1);
-        modeClient  = nh.serviceClient<uranus_dp::ToggleControlMode>("toggle_control_mode");
+        modeClient  = nh.serviceClient<uranus_dp::SetControlMode>("set_control_mode");
 
-        control_mode = joystick::DirectionalInput::OPEN_LOOP;
+        control_mode = ControlModes::OPEN_LOOP;
     }
 
     void callback(const joystick::DirectionalInput &joy_msg)
     {
         if (joy_msg.control_mode != control_mode)
         {
-            control_mode = joy_msg.control_mode;
-            uranus_dp::ToggleControlMode srv;
+            control_mode = static_cast<ControlMode>(joy_msg.control_mode);
+            uranus_dp::SetControlMode srv;
+            srv.request.mode = control_mode;
             if (!modeClient.call(srv))
             {
-                ROS_ERROR_STREAM("Failed to call service toggle_control_mode. New mode " << controlModeString(control_mode) << " not activated.");
+                ROS_ERROR_STREAM("Failed to call service set_control_mode. New mode " << control_mode << " not activated.");
             }
         }
 
-        if (control_mode == joystick::DirectionalInput::OPEN_LOOP)
+        if (control_mode == ControlModes::OPEN_LOOP)
         {
             geometry_msgs::Wrench setpoint_msg;
             setpoint_msg.force.x  = joy_msg.strafe_X * NORMALIZATION * SCALING_LIN;
@@ -41,7 +43,7 @@ public:
             setpoint_msg.torque.z = joy_msg.turn_Y * NORMALIZATION * SCALING_ANG;
             wrenchPub.publish(setpoint_msg);
         }
-        else if (control_mode == joystick::DirectionalInput::STATIONKEEPING)
+        else if (control_mode == ControlModes::STATIONKEEPING)
         {
             // Todo: Actually populate the pose message with values
             geometry_msgs::Pose setpoint_msg;
@@ -67,21 +69,21 @@ private:
     ros::Publisher     posePub;
     ros::ServiceClient modeClient;
 
-    int control_mode;
+    ControlMode control_mode;
 
     static const double NORMALIZATION = 0.000030517578125; // Scale joystick inputs down to [-1, 1]
     static const double SCALING_LIN   = 10;                // Scale forces up to [-10, 10] (Newton)
     static const double SCALING_ANG   = 2;                 // Scale torques up to [-2, 2] (Newton meters)
 
-    std::string controlModeString(int mode)
-    {
-        if (mode == joystick::DirectionalInput::OPEN_LOOP)
-            return "open loop";
-        else if (mode == joystick::DirectionalInput::STATIONKEEPING)
-            return "stationkeeping";
-        else
-            return "invalid mode";
-    }
+    // std::string controlModeString(int mode)
+    // {
+    //     if (mode == joystick::DirectionalInput::OPEN_LOOP)
+    //         return "open loop";
+    //     else if (mode == joystick::DirectionalInput::STATIONKEEPING)
+    //         return "stationkeeping";
+    //     else
+    //         return "invalid mode";
+    // }
 };
 
 int main(int argc, char** argv){

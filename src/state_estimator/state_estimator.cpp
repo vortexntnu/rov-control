2018767ca81f8270b8ea040bbd874bfdfc1ffd90
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-15 14:24:51 +0200

    Comment out kalman filter

--- src/state_estimator/state_estimator.cpp
+++ src/state_estimator/state_estimator.cpp
@@ -6,13 +6,13 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "state_estimator");
 
     double frequency = 100; // Parameter server
-    ExtendedKalmanFilter estimator(1/frequency);
+    // ExtendedKalmanFilter estimator(1/frequency);
 
     ros::Rate loop_rate(frequency);
     while (ros::ok())
     {
         ros::spinOnce();
-        estimator.update();
+        // estimator.update();
         loop_rate.sleep();
     }
 }

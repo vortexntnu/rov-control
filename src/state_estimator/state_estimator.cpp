8feddc4079c39e94593c66feafa6a6cb36ee23c9
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-15 14:55:58 +0200

    Run integration filter from state estimator node

--- src/state_estimator/state_estimator.cpp
+++ src/state_estimator/state_estimator.cpp
@@ -1,12 +1,15 @@
 #include "ros/ros.h"
 #include "extended_kalman_filter.h"
+#include "integration_filter.h"
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "state_estimator");
+    ROS_INFO("Launching node state_estimator.");
 
     double frequency = 100; // Parameter server
     // ExtendedKalmanFilter estimator(1/frequency);
+    IntegrationFilter estimator;
 
     ros::Rate loop_rate(frequency);
     while (ros::ok())

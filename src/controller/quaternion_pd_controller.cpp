9c6827d09ae69242b6157724783867161216d97f
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-16 16:38:17 +0200

    Better init and a lot of debug printing

--- src/controller/quaternion_pd_controller.cpp
+++ src/controller/quaternion_pd_controller.cpp
@@ -2,6 +2,8 @@
 
 #include "quaternion_pd_controller.h"
 
+// void Print
+
 QuaternionPdController::QuaternionPdController()
 {
     stateSub    = nh.subscribe("state_estimate", 10, &QuaternionPdController::stateCallback, this);
@@ -9,17 +11,18 @@ QuaternionPdController::QuaternionPdController()
     controlPub  = nh.advertise<geometry_msgs::Wrench>("rov_forces", 10);
     enabled = false;
 
-    // Initial values
-    p         << 0, 0, 0;
-    q.w()     =  0;
-    q.vec()   << 0, 0, 0;
-    nu        << 0, 0, 0, 0, 0, 0;
-    p_d       << 0, 0, 0;
-    q_d.w()   =  0;
-    q_d.vec() << 0, 0, 0;
-    tau       << -1, -1, -1, -1, -1, -1;
+    // Initialize all values to zero/identity
+    p.setZero();
+    q.setIdentity();
+    nu.setZero();
+    p_d.setZero();
+    q_d.setIdentity();
+    z.setZero();
+    tau.setZero();
+    g.setZero();
+    R = q.toRotationMatrix();
 
-    // Gains etc. (from paper, temporary)
+    // Gains etc. (temporary values from paper)
     K_D = Eigen::MatrixXd::Identity(6,6);
     K_p = 30*Eigen::MatrixXd::Identity(3,3);
     c = 200;
@@ -31,18 +34,31 @@ QuaternionPdController::QuaternionPdController()
 
 void QuaternionPdController::stateCallback(const uranus_dp::State &msg)
 {
+    // ROS_INFO("quaternion_pd_controller: stateCallback running.");
+
     tf::pointMsgToEigen(msg.pose.position, p);
     tf::quaternionMsgToEigen(msg.pose.orientation, q);
     tf::twistMsgToEigen(msg.twist, nu);
 
-    // Update rotation matrix
+    // ROS_INFO_STREAM("p set to [" << p(0) << ", " << p(1) << ", " << p(2) << "].");
+    // ROS_INFO_STREAM("q set to [" << q.w() << ", " << q.x() << ", " << q.y() << ", " << q.z() << "].");
+    // ROS_INFO_STREAM("nu set to [" << nu(0) << ", " << nu(1) << ", " << nu(2) << ", " << nu(3) << ", " << nu(4) << ", " << nu(5) << "].");
+
     R = q.toRotationMatrix();
+    // ROS_INFO_STREAM("R = [" << R(0,0) << ", " << R(0,1) << ", " << R(0,2) << "]");
+    // ROS_INFO_STREAM("R = [" << R(1,0) << ", " << R(1,1) << ", " << R(1,2) << "]");
+    // ROS_INFO_STREAM("R = [" << R(2,0) << ", " << R(2,1) << ", " << R(2,2) << "]");
 }
 
 void QuaternionPdController::setpointCallback(const geometry_msgs::Pose &msg)
 {
+    // ROS_INFO("quaternion_pd_controller: setpointCallback running.");
+
     tf::pointMsgToEigen(msg.position, p_d);
     tf::quaternionMsgToEigen(msg.orientation, q_d);
+
+    // ROS_INFO_STREAM("p_d set to [" << p_d(0) << ", " << p_d(1) << ", " << p_d(2) << "].");
+    // ROS_INFO_STREAM("q_d set to [" << q_d.w() << ", " << q_d.x() << ", " << q_d.y() << ", " << q_d.z() << "].");
 }
 
 void QuaternionPdController::compute()
@@ -54,6 +70,10 @@ void QuaternionPdController::compute()
         updateRestoringForceVector();
 
         tau = - K_D*nu - K_P*z + g;
+        // ROS_INFO_STREAM("compute(): nu = [" << nu(0) << ", " << nu(1) << ", " << nu(2) << ", " << nu(3) << ", " << nu(4) << ", " << nu(5) << "].");
+        // ROS_INFO_STREAM("compute(): z = [" << z(0) << ", " << z(1) << ", " << z(2) << ", " << z(3) << ", " << z(4) << ", " << z(5) << "].");
+        // ROS_INFO_STREAM("compute(): g = [" << g(0) << ", " << g(1) << ", " << g(2) << "].");
+        // ROS_INFO_STREAM("tau =  [" << tau(0) << ", " << tau(1) << ", " << tau(2) << ", " << tau(3) << ", " << tau(4) << ", " << tau(5) << "].");
 
         geometry_msgs::Wrench msg;
         tf::wrenchEigenToMsg(tau, msg);
@@ -92,6 +112,9 @@ void QuaternionPdController::updateRestoringForceVector()
     // Should I assume an updated R, or force an update here?
     Eigen::Vector3d f_g = R.transpose() * Eigen::Vector3d(0, 0, W);
     Eigen::Vector3d f_b = R.transpose() * Eigen::Vector3d(0, 0, -B);
+
+    // ROS_INFO_STREAM("updateRestoringForceVector(): f_g = [" << f_g(0) << ", " << f_g(1) << ", " << f_g(2) << "].");
+    // ROS_INFO_STREAM("updateRestoringForceVector(): f_b = [" << f_b(0) << ", " << f_b(1) << ", " << f_b(2) << "].");
 
     g << f_g + f_b,
          r_g.cross(f_g) + r_b.cross(f_b);

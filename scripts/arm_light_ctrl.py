1ad04e9596b1fefd5de93902cd45d31517ce06f0
peteraaser <peteraaser@gmail.com>
2016-04-20 13:57:15 +0200

    Unfinished code for arm and light

---
+++ scripts/arm_light_ctrl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+from joystick.msg import Joystick
+from joystick.msg import PwmRequests
+from joystick.msg import DirectionalInput
+
+# class ArmLightNode:
+# 
+#     def __init__(self):
+#         rospy.init_node("arm_light_ctrl_node", anonymous=False)
+# 
+#         self.light = 
+
+

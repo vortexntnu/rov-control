b66f8c2d2131f72adbaa739471de78503c4ea2dd
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-23 15:58:18 +0200

    Comment out annoying debug print

--- src/allocator/lagrange_allocator.cpp
+++ src/allocator/lagrange_allocator.cpp
@@ -74,7 +74,7 @@ void LagrangeAllocator::callback(const geometry_msgs::Wrench& tauMsg)
     uMsg.F6 = u(5);
     pub.publish(uMsg);
 
-    ROS_INFO_STREAM("lagrange_allocator: Sending " << u(0) << ", " << u(1) << ", " << u(2) << ", " << u(3) << ", " << u(4) << ", " << u(5));
+    // ROS_INFO_STREAM("lagrange_allocator: Sending " << u(0) << ", " << u(1) << ", " << u(2) << ", " << u(3) << ", " << u(4) << ", " << u(5));
 }
 
 void LagrangeAllocator::setWeights(const Eigen::MatrixXd &W_new)

fd9042f2c6c5c02dc1aa3deb08782e2bb5224d23
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-24 21:48:20 +0200

    Remove old stuff

--- src/setpoint_processing.cpp
+++ src/setpoint_processing.cpp
@@ -2,7 +2,6 @@
 #include "geometry_msgs/Wrench.h"
 #include "geometry_msgs/Pose.h"
 #include <maelstrom_msgs/JoystickMotionCommand.h>
-#include <string>
 #include "uranus_dp/SetControlMode.h"
 #include "control_mode_enum.h"
 
@@ -19,7 +18,6 @@ public:
         control_mode = ControlModes::OPEN_LOOP;
     }
 
-    // void callback(const maelstrom_msgs::DirectionalInput &msg)
     void callback(const maelstrom_msgs::JoystickMotionCommand& msg)
     {
         if (msg.control_mode != control_mode)
@@ -28,21 +26,13 @@ public:
             uranus_dp::SetControlMode srv;
             srv.request.mode = control_mode;
             if (!modeClient.call(srv))
-            {
                 ROS_ERROR_STREAM("Failed to call service set_control_mode. New mode " << control_mode << " not activated.");
-            }
         }
 
         if (control_mode == ControlModes::OPEN_LOOP)
         {
             ROS_INFO("setpoint_processing: Sending open loop setpoints.");
             geometry_msgs::Wrench setpoint_msg;
-            // setpoint_msg.force.x  =  msg.strafe_Y * NORMALIZE_16BIT * MAX_FORCE;
-            // setpoint_msg.force.y  =  msg.strafe_X * NORMALIZE_16BIT * MAX_FORCE;
-            // setpoint_msg.force.z  = -msg.ascend   * NORMALIZE_09BIT * MAX_FORCE;
-            // setpoint_msg.torque.x =  0;
-            // setpoint_msg.torque.y =  msg.turn_Y * NORMALIZE_16BIT * MAX_TORQUE;
-            // setpoint_msg.torque.z =  msg.turn_X * NORMALIZE_16BIT * MAX_TORQUE;
             setpoint_msg.force.x  = msg.forward * MAX_FORCE;
             setpoint_msg.force.y  = msg.right   * MAX_FORCE;
             setpoint_msg.force.z  = msg.down    * MAX_FORCE;
@@ -80,10 +70,8 @@ private:
 
     ControlMode control_mode;
 
-    // static const double NORMALIZE_16BIT = 0.000030517578125; // Scaling from 16 bit signed int to [-1, 1]
-    // static const double NORMALIZE_09BIT = 0.00390625;        // Scaling from [-255, 255] to [-1, 1]
-    static const double MAX_FORCE       = 10;                // Scale forces up to [-10, 10] (Newton)
-    static const double MAX_TORQUE      = 5;                 // Scale torques up to [-5, 5] (Newton meters)
+    static const double MAX_FORCE  = 10;                // Scale forces up to [-10, 10] (Newton)
+    static const double MAX_TORQUE = 5;                 // Scale torques up to [-5, 5] (Newton meters)
 };
 
 int main(int argc, char** argv){

dbc2068440df5cda9d8696352501f79946e58a70
peteraaser <peteraaser@gmail.com>
2016-04-25 11:57:06 +0200

    Increase deadzone for joysticks. Add deadzone for triggers. Adjust response to prevent response from going from 0 to deadzone threshold when crossing deadzone threshold

--- scripts/hmi.py
+++ scripts/hmi.py
@@ -27,7 +27,7 @@ class HmiNode:
 
     def joystick_callback(self, joystick):
         
-        self.directional_input.forward      =       joystick.strafe_Y                       /   XBOX_JOYSTICK_RANGE
+        self.directional_input.forward      =       -joystick.strafe_Y                      /   XBOX_JOYSTICK_RANGE
         self.directional_input.right        =       joystick.strafe_X                       /   XBOX_JOYSTICK_RANGE
         self.directional_input.turn_right   =       joystick.turn_X                         /   XBOX_JOYSTICK_RANGE
         self.directional_input.tilt_up      =       joystick.turn_Y                         /   XBOX_JOYSTICK_RANGE

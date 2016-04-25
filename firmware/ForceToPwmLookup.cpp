a266f720f9fae020df719b1f34c0ffd1ddf67d57
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-25 18:18:01 +0200

    write linear interpolation

--- firmware/ForceToPwmLookup.cpp
+++ firmware/ForceToPwmLookup.cpp
@@ -1,32 +1,31 @@
 #include "ForceToPwmLookup.h"
 #include <Arduino.h>
 
-int ForceToPwm(float force) {
+int ForceToPwm(float force)
+{
   return MicroSecToPwmValue(ForceToMicroSec(force));
 }
 
-
-int ForceToMicroSec(float force) {
-
-  int us = PwmUpperMicroSec;
-
-  
-  for(int i = 0; i < FoceToPwmLookupTableSize; i++) {
-
-    if( force < FoceToPwmLookupTable[i] ) {
-
-      us = i * PwmIncremetMicroSec + PwmLowerMicroSec;
-      
+int ForceToMicroSec(float force)
+{
+  double us = PwmUpperMicroSec;
+  for (int i = 0; i < ForceLookupSize; ++i)
+  {
+    if (force < ForceLookup[i])
+    {
+      double us_prev = PwmLowerMicroSec + PwmIncremetMicroSec*(i-1);
+      double us_next = PwmLowerMicroSec + PwmIncremetMicroSec*i;
+      double us_diff = us_next - us_prev;
+      double force_diff = ForceLookup[i] - ForceLookup[i-1];
+      double force_offset = force - ForceLookup[i-1];
+      us = us_prev + us_diff*(force_offset/force_diff);
       break;
     }
-    
   }
-  
   return us;
-  
 }
 
-int MicroSecToPwmValue(int us) {
-  return map(us, PwmLowerMicroSec, PwmUpperMicroSec,
-	     PwmLowerValue, PwmUpperValue);
+int MicroSecToPwmValue(int us)
+{
+  return map(us, PwmLowerMicroSec, PwmUpperMicroSec, PwmLowerValue, PwmUpperValue);
 }

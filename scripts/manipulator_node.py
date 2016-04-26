28f218464c00ab6384129744d145c8843d72349d
Hawkon <hakongimse@gmail.com>
2016-04-26 20:41:57 +0200

    two servos at base

--- scripts/manipulator_node.py
+++ scripts/manipulator_node.py
@@ -19,21 +19,25 @@ class ManipulatorNode():
 
 	self.grip_angle = 0
 	self.base_angle = 0
+	self.base2_angle = 0
 	self.rot_angle = 0
 
 	GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
 	# Control signal from pin 11, 13 and 15
-        pwmpin_grip, pwmpin_base, pwmpin_rot = 11, 13, 15
+        pwmpin_grip, pwmpin_base, pwmpin_base2, pwmpin_rot = 11, 15, 16, 13
         GPIO.setup(pwmpin_grip, GPIO.OUT)
         GPIO.setup(pwmpin_base, GPIO.OUT)
+        GPIO.setup(pwmpin_base2, GPIO.OUT)
         GPIO.setup(pwmpin_rot, GPIO.OUT)
         # create an object for PWM control at 100 Hz
         self.pwm_grip_control = GPIO.PWM(pwmpin_grip, 100)
         self.pwm_base_control = GPIO.PWM(pwmpin_base, 100)
+        self.pwm_base2_control = GPIO.PWM(pwmpin_base2, 100)
         self.pwm_rot_control = GPIO.PWM(pwmpin_rot, 100)
         self.pwm_grip_control.start(0)
         self.pwm_base_control.start(0)
+        self.pwm_base2_control.start(0)
         self.pwm_rot_control.start(0)
         self.control_listener()
 
@@ -54,13 +58,16 @@ class ManipulatorNode():
         self.pwm_grip_control.ChangeDutyCycle(grip_duty)
 	# Base
 	self.base_angle = min(max(self.base_angle + ManipulatorNode.arm_increment_value * self.model.arm_base, 1), 179)
+	self.base2_angle = 180 - self.base_angle
 	base_duty = float(self.base_angle) / 10.0 + 2.5
+	base2_duty = float(self.base2_angle) / 10.0 + 2.5
         self.pwm_base_control.ChangeDutyCycle(base_duty)
+        self.pwm_base2_control.ChangeDutyCycle(base2_duty)
 	# Rotation
 	self.rot_angle = min(max(self.rot_angle + ManipulatorNode.arm_increment_value * self.model.arm_rot, 1), 179)
 	rot_duty = float(self.rot_angle) / 10.0 + 2.5
         self.pwm_rot_control.ChangeDutyCycle(rot_duty)
-	print self.grip_angle, self.base_angle, self.rot_angle
+	print self.grip_angle, self.base_angle, self.base2_angle, self.rot_angle
 
 
 if __name__ == '__main__':

7f094cba0898177355502ed3b03f10479ef0599f
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-04-30 12:48:48 +0200

    Split controller into node and class implementation

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -1,31 +1,22 @@
-#include "ros/ros.h"
-#include "open_loop_controller.h"
-#include "depth_hold_controller.h"
-#include "quaternion_pd_controller.h"
-#include "uranus_dp/SetControlMode.h"
-#include "uranus_dp/SetControllerGains.h"
-#include "../control_mode_enum.h"
+#include "controller.h"
 
-class Controller
+Controller::Controller(unsigned int f)
 {
-public:
-    Controller(unsigned int f)
-    {
-        frequency = f;
-        control_mode = ControlModes::OPEN_LOOP;
-        position_hold_controller.disable();
-        depth_hold_controller.disable();
-        open_loop_controller.enable();
-    }
+    frequency = f;
+    control_mode = ControlModes::OPEN_LOOP;
+    position_hold_controller.disable();
+    depth_hold_controller.disable();
+    open_loop_controller.enable();
+}
 
-    bool setControlMode(uranus_dp::SetControlMode::Request &req, uranus_dp::SetControlMode::Response &resp)
+bool Controller::setControlMode(uranus_dp::SetControlMode::Request &req, uranus_dp::SetControlMode::Response &resp)
+{
+    ControlMode new_control_mode = static_cast<ControlMode>(req.mode);
+    if (new_control_mode != control_mode)
     {
-        ControlMode new_control_mode = static_cast<ControlMode>(req.mode);
-        if (new_control_mode != control_mode)
+        control_mode = new_control_mode;
+        switch (control_mode)
         {
-            control_mode = new_control_mode;
-            switch (control_mode)
-            {
             case ControlModes::OPEN_LOOP:
             ROS_INFO("Changing control mode to OPEN LOOP.");
             position_hold_controller.disable();
@@ -50,56 +41,29 @@ public:
             default:
             ROS_WARN("Invalid control mode set.");
             break;
-            }
         }
-        else
-        {
-            ROS_INFO("Attempt to set already active control mode, ignoring.");
-        }
-        return true;
     }
-
-    bool setControllerGains(uranus_dp::SetControllerGains::Request &req, uranus_dp::SetControllerGains::Response &resp)
+    else
     {
-        ROS_INFO_STREAM("Setting new gains: a = " << req.a << ", b = " << req.b << ", c = " << req.c << ".");
-        position_hold_controller.setGains(req.a, req.b, req.c);
+        ROS_INFO("Attempt to set already active control mode, ignoring.");
     }
+    return true;
+}
 
-    void run()
-    {
-        ros::Rate rate(frequency);
-        while (ros::ok())
-        {
-            ros::spinOnce();
-            position_hold_controller.compute();
-            depth_hold_controller.compute();
-            rate.sleep();
-        }
-    }
-private:
-    ros::NodeHandle nh;
-    unsigned int frequency;
-
-    ControlMode control_mode;
-    OpenLoopController     open_loop_controller;
-    QuaternionPdController position_hold_controller;
-    DepthHoldController    depth_hold_controller;
-};
-
-
-
-int main(int argc, char **argv)
+bool Controller::setControllerGains(uranus_dp::SetControllerGains::Request &req, uranus_dp::SetControllerGains::Response &resp)
 {
-    ros::init(argc, argv, "controller");
-    ROS_INFO("Launching node controller.");
-    ros::NodeHandle nh;
+    ROS_INFO_STREAM("Setting new gains: a = " << req.a << ", b = " << req.b << ", c = " << req.c << ".");
+    position_hold_controller.setGains(req.a, req.b, req.c);
+}
 
-    unsigned int frequency = 10;
-    Controller controller(frequency);
-    ros::ServiceServer ss1 = nh.advertiseService("set_control_mode", &Controller::setControlMode, &controller);
-    ros::ServiceServer ss2 = nh.advertiseService("set_controller_gains", &Controller::setControllerGains, &controller);
-
-    controller.run();
-    ROS_ERROR("controller.run() has returned. That's not meant to happen.");
-    return 0;
+void Controller::spin()
+{
+    ros::Rate rate(frequency);
+    while (ros::ok())
+    {
+        ros::spinOnce();
+        position_hold_controller.compute();
+        depth_hold_controller.compute();
+        rate.sleep();
+    }
 }

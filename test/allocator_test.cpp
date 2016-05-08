f833638c6245dbcb28d1abb2fe86c0d8c48bc50f
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-05-08 18:45:14 +0200

    Update thruster names

--- test/allocator_test.cpp
+++ test/allocator_test.cpp
@@ -37,12 +37,12 @@ public:
             ros::spinOnce();
     }
 
-    double F_A;
-    double F_B;
-    double F_C;
-    double F_D;
-    double F_E;
-    double F_F;
+    double F1;
+    double F2;
+    double F3;
+    double F4;
+    double F5;
+    double F6;
 
     static const double MAX_ERROR = 1e-6; // Max absolute error 1 micronewton
 
@@ -54,12 +54,12 @@ public:
 
     void Callback(const maelstrom_msgs::ThrusterForces& msg)
     {
-        F_A = msg.F1;
-        F_B = msg.F2;
-        F_C = msg.F3;
-        F_D = msg.F4;
-        F_E = msg.F5;
-        F_F = msg.F6;
+        F1 = msg.F1;
+        F2 = msg.F2;
+        F3 = msg.F3;
+        F4 = msg.F4;
+        F5 = msg.F5;
+        F6 = msg.F6;
         message_received = true;
     }
 
@@ -80,12 +80,12 @@ TEST_F(AllocatorTest, ZeroInput)
     Publish(0, 0, 0, 0, 0, 0);
     WaitForMessage();
 
-    EXPECT_NEAR(F_A, 0, MAX_ERROR);
-    EXPECT_NEAR(F_B, 0, MAX_ERROR);
-    EXPECT_NEAR(F_C, 0, MAX_ERROR);
-    EXPECT_NEAR(F_D, 0, MAX_ERROR);
-    EXPECT_NEAR(F_E, 0, MAX_ERROR);
-    EXPECT_NEAR(F_F, 0, MAX_ERROR);
+    EXPECT_NEAR(F1, 0, MAX_ERROR);
+    EXPECT_NEAR(F2, 0, MAX_ERROR);
+    EXPECT_NEAR(F3, 0, MAX_ERROR);
+    EXPECT_NEAR(F4, 0, MAX_ERROR);
+    EXPECT_NEAR(F5, 0, MAX_ERROR);
+    EXPECT_NEAR(F6, 0, MAX_ERROR);
 }
 
 TEST_F(AllocatorTest, Forward)
@@ -93,12 +93,12 @@ TEST_F(AllocatorTest, Forward)
     Publish(1, 0, 0, 0, 0, 0);
     WaitForMessage();
 
-    EXPECT_TRUE(F_A < 0);
-    EXPECT_TRUE(F_B > 0);
-    EXPECT_TRUE(F_C < 0);
-    EXPECT_TRUE(F_D > 0);
-    EXPECT_TRUE(F_E > 0);
-    EXPECT_TRUE(F_F < 0);
+    EXPECT_TRUE(F1 < 0);
+    EXPECT_TRUE(F2 > 0);
+    EXPECT_TRUE(F3 < 0);
+    EXPECT_TRUE(F4 > 0);
+    EXPECT_TRUE(F5 > 0);
+    EXPECT_TRUE(F6 < 0);
 }
 
 TEST_F(AllocatorTest, Sideways)
@@ -106,12 +106,12 @@ TEST_F(AllocatorTest, Sideways)
     Publish(0, 1, 0, 0, 0, 0);
     WaitForMessage();
 
-    EXPECT_NEAR(F_A, 0, MAX_ERROR);
-    EXPECT_TRUE(F_B < 0);
-    EXPECT_TRUE(F_C < 0);
-    EXPECT_NEAR(F_D, 0, MAX_ERROR);
-    EXPECT_TRUE(F_E > 0);
-    EXPECT_TRUE(F_F > 0);
+    EXPECT_NEAR(F1, 0, MAX_ERROR);
+    EXPECT_TRUE(F2 < 0);
+    EXPECT_TRUE(F3 < 0);
+    EXPECT_NEAR(F4, 0, MAX_ERROR);
+    EXPECT_TRUE(F5 > 0);
+    EXPECT_TRUE(F6 > 0);
 }
 
 TEST_F(AllocatorTest, Downward)
@@ -119,12 +119,12 @@ TEST_F(AllocatorTest, Downward)
     Publish(0, 0, 1, 0, 0, 0);
     WaitForMessage();
 
-    EXPECT_TRUE(F_A > 0);
-    EXPECT_NEAR(F_B, 0, MAX_ERROR);
-    EXPECT_NEAR(F_C, 0, MAX_ERROR);
-    EXPECT_TRUE(F_D > 0);
-    EXPECT_NEAR(F_E, 0, MAX_ERROR);
-    EXPECT_NEAR(F_F, 0, MAX_ERROR);
+    EXPECT_TRUE(F1 > 0);
+    EXPECT_NEAR(F2, 0, MAX_ERROR);
+    EXPECT_NEAR(F3, 0, MAX_ERROR);
+    EXPECT_TRUE(F4 > 0);
+    EXPECT_NEAR(F5, 0, MAX_ERROR);
+    EXPECT_NEAR(F6, 0, MAX_ERROR);
 }
 
 TEST_F(AllocatorTest, TiltUp)
@@ -132,12 +132,12 @@ TEST_F(AllocatorTest, TiltUp)
     Publish(0, 0, 0, 0, 1, 0);
     WaitForMessage();
 
-    EXPECT_TRUE(F_A < 0);
-    EXPECT_NEAR(F_B, 0, MAX_ERROR);
-    EXPECT_NEAR(F_C, 0, MAX_ERROR);
-    EXPECT_TRUE(F_D > 0);
-    EXPECT_NEAR(F_E, 0, MAX_ERROR);
-    EXPECT_NEAR(F_F, 0, MAX_ERROR);
+    EXPECT_TRUE(F1 < 0);
+    EXPECT_NEAR(F2, 0, MAX_ERROR);
+    EXPECT_NEAR(F3, 0, MAX_ERROR);
+    EXPECT_TRUE(F4 > 0);
+    EXPECT_NEAR(F5, 0, MAX_ERROR);
+    EXPECT_NEAR(F6, 0, MAX_ERROR);
 }
 
 TEST_F(AllocatorTest, TurnRight)
@@ -145,12 +145,12 @@ TEST_F(AllocatorTest, TurnRight)
     Publish(0, 0, 0, 0, 0, 1);
     WaitForMessage();
 
-    EXPECT_NEAR(F_A, 0, MAX_ERROR);
-    EXPECT_TRUE(F_B < 0);
-    EXPECT_TRUE(F_C > 0);
-    EXPECT_NEAR(F_D, 0, MAX_ERROR);
-    EXPECT_TRUE(F_E > 0);
-    EXPECT_TRUE(F_F < 0);
+    EXPECT_NEAR(F1, 0, MAX_ERROR);
+    EXPECT_TRUE(F2 < 0);
+    EXPECT_TRUE(F3 > 0);
+    EXPECT_NEAR(F4, 0, MAX_ERROR);
+    EXPECT_TRUE(F5 > 0);
+    EXPECT_TRUE(F6 < 0);
 }
 
 int main(int argc, char **argv)

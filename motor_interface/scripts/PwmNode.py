c7441e5a26c31fd9247bef4b88d0e8d86e856257
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-10-01 12:04:48 +0200

    Clean formatting and update comments

--- motor_interface/scripts/PwmNode.py
+++ motor_interface/scripts/PwmNode.py
@@ -1,26 +1,15 @@
-#!/bin/python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import ForceToPwmLookup
-
 import Adafruit_PCA9685
-
 import rospy
-
 
 from vortex_msgs.msg import ThrusterForces
 from vortex_msgs.msg import PwmRequests
 
-
-def callback_arm(data):
-    #
-    #
-    print ''
-
-
 def callback_thruster(ForceInput):
-
-    #Gjør klar statusmelding og regn om kraft til pwm
+    # Publish status message
     PwmStatusMsg.pwm1 = int(ForceToPwmLookup.ForceToPwm(ForceInput.F1))
     PwmStatusMsg.pwm2 = int(ForceToPwmLookup.ForceToPwm(ForceInput.F2))
     PwmStatusMsg.pwm3 = int(ForceToPwmLookup.ForceToPwm(ForceInput.F3))
@@ -29,7 +18,7 @@ def callback_thruster(ForceInput):
     PwmStatusMsg.pwm6 = int(ForceToPwmLookup.ForceToPwm(ForceInput.F6))
     PwmStatusPub.publish(PwmStatusMsg)
 
-    #Sett pwm
+    # Set PWM outputs
     pwm.set_pwm(0, 0, PwmStatusMsg.pwm1)
     pwm.set_pwm(1, 0, PwmStatusMsg.pwm2)
     pwm.set_pwm(2, 0, PwmStatusMsg.pwm3)
@@ -37,41 +26,32 @@ def callback_thruster(ForceInput):
     pwm.set_pwm(4, 0, PwmStatusMsg.pwm5)
     pwm.set_pwm(5, 0, PwmStatusMsg.pwm6)
 
-
 def init_pwm():
     # Initialise the PCA9685 using the default address (0x40).
     pwm = Adafruit_PCA9685.PCA9685()
-    #sett frekvensen
+
+    # 260 Hz gives a pulse length of roughly 3800 microseconds
+    # Our motor controllers require min. 2000 microseconds pulse length
     pwm.set_pwm_freq(260)
 
-    #Til å sende debugmeldinger
+    # Til å sende debugmeldinger
     PwmStatusMsg = PwmRequests()
 
-    #sett alle motorene til 0 newton
-    pwm_null_newton = int(ForceToPwmLookup.ForceToPwm(0))
-
-    pwm.set_pwm(0, 0, pwm_null_newton)
-    pwm.set_pwm(1, 0, pwm_null_newton)
-    pwm.set_pwm(2, 0, pwm_null_newton)
-    pwm.set_pwm(3, 0, pwm_null_newton)
-    pwm.set_pwm(4, 0, pwm_null_newton)
-    pwm.set_pwm(5, 0, pwm_null_newton)
-
+    # Initialize all thrusters to 0 newton
+    pwm_zero_newton = int(ForceToPwmLookup.ForceToPwm(0))
+    pwm.set_pwm(0, 0, pwm_zero_newton)
+    pwm.set_pwm(1, 0, pwm_zero_newton)
+    pwm.set_pwm(2, 0, pwm_zero_newton)
+    pwm.set_pwm(3, 0, pwm_zero_newton)
+    pwm.set_pwm(4, 0, pwm_zero_newton)
+    pwm.set_pwm(5, 0, pwm_zero_newton)
 
 if __name__ == '__main__':
-
     rospy.init_node('PwmNode', anonymous=True)
     init_pwm()
 
-
     PwmStatusPub = rospy.Publisher('pwm_status', PwmRequests, queue_size=10)
-
-
-    #rospy.Subscriber("joystick_arm_command", JoystickArmCommand, callback)
     rospy.Subscriber("thruster_forces", ThrusterForces, callback_thruster)
 
     print "PwmNode er klar"
-
     rospy.spin()
-
-

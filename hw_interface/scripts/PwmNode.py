98a0a36fca391645383a5dd3c6b5603501a843dc
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-10-01 11:29:18 +0200

    Remove deprecated sensor interfacing
    The hw_interface package is currently only used for thruster interfacing,
    while sensor interfacing has been moved to the sensor_interface package.

--- hw_interface/scripts/PwmNode.py
+++ hw_interface/scripts/PwmNode.py
@@ -8,9 +8,8 @@ import Adafruit_PCA9685
 import rospy
 
 
-#from maelstrom_msgs.msg import JoystickArmCommand
-from maelstrom_msgs.msg import ThrusterForces
-from maelstrom_msgs.msg import PwmRequests
+from vortex_msgs.msg import ThrusterForces
+from vortex_msgs.msg import PwmRequests
 
 
 def callback_arm(data):
@@ -36,7 +35,7 @@ def callback_thruster(ForceInput):
     pwm.set_pwm(2, 0, PwmStatusMsg.pwm3)
     pwm.set_pwm(3, 0, PwmStatusMsg.pwm4)
     pwm.set_pwm(4, 0, PwmStatusMsg.pwm5)
-    pwm.set_pwm(5, 0, PwmStatusMsg.pwm6) 
+    pwm.set_pwm(5, 0, PwmStatusMsg.pwm6)
 
 
 def init_pwm():
@@ -45,9 +44,9 @@ def init_pwm():
     #sett frekvensen
     pwm.set_pwm_freq(260)
 
-    #Til å sende debugmeldinger 
+    #Til å sende debugmeldinger
     PwmStatusMsg = PwmRequests()
-    
+
     #sett alle motorene til 0 newton
     pwm_null_newton = int(ForceToPwmLookup.ForceToPwm(0))
 
@@ -57,22 +56,22 @@ def init_pwm():
     pwm.set_pwm(3, 0, pwm_null_newton)
     pwm.set_pwm(4, 0, pwm_null_newton)
     pwm.set_pwm(5, 0, pwm_null_newton)
-    
+
 
 if __name__ == '__main__':
-    
+
     rospy.init_node('PwmNode', anonymous=True)
     init_pwm()
-    
-    
+
+
     PwmStatusPub = rospy.Publisher('pwm_status', PwmRequests, queue_size=10)
-    
-    
+
+
     #rospy.Subscriber("joystick_arm_command", JoystickArmCommand, callback)
     rospy.Subscriber("thruster_forces", ThrusterForces, callback_thruster)
 
     print "PwmNode er klar"
-    
+
     rospy.spin()
-    
-        
+
+

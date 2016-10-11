0ccc2a87b32e034337c66ee5526cd2b471ef7feb
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-10-11 22:59:02 +0200

    Comment back in I2C code
    Was commented out for debugging independently of the PCA9685 hardware.

--- motor_interface/scripts/motor_interface.py
+++ motor_interface/scripts/motor_interface.py
@@ -15,7 +15,6 @@ NEUTRAL_PULSE_WIDTH           = interp(0, lookup_table.thruster_force, lookup_ta
 
 def pulse_width_in_bits(force):
     pulse_width_in_microseconds = interp(force, lookup_table.thruster_force, lookup_table.pulse_width)
-    return int(round(pulse_width_in_microseconds))
     normalized_duty_cycle = pulse_width_in_microseconds/PERIOD_LENGTH_IN_MICROSECONDS
     return int(round(BITS_PER_PERIOD * normalized_duty_cycle))
 
@@ -31,36 +30,32 @@ def callback(desired_forces):
     pwm_state.pwm6 = pulse_width_in_bits(desired_forces.F6)
 
     # Set PWM outputs
-    # pca9685.set_pwm(0, 0, pwm_state.pwm1)
-    # pca9685.set_pwm(1, 0, pwm_state.pwm2)
-    # pca9685.set_pwm(2, 0, pwm_state.pwm3)
-    # pca9685.set_pwm(3, 0, pwm_state.pwm4)
-    # pca9685.set_pwm(4, 0, pwm_state.pwm5)
-    # pca9685.set_pwm(5, 0, pwm_state.pwm6)
+    pca9685.set_pwm(0, 0, pwm_state.pwm1)
+    pca9685.set_pwm(1, 0, pwm_state.pwm2)
+    pca9685.set_pwm(2, 0, pwm_state.pwm3)
+    pca9685.set_pwm(3, 0, pwm_state.pwm4)
+    pca9685.set_pwm(4, 0, pwm_state.pwm5)
+    pca9685.set_pwm(5, 0, pwm_state.pwm6)
 
     # Publish outputs for debug
     pub.publish(pwm_state)
 
 def init():
-    pass
     # Initialise the PCA9685 using the default address (0x40)
-    # pca9685 = Adafruit_PCA9685.PCA9685()
+    pca9685 = Adafruit_PCA9685.PCA9685()
 
     # 260 Hz gives a pulse length of roughly 3800 microseconds
     # Our motor controllers require min. 2000 microseconds pulse length
     # Max frequency is 500 Hz
-    # pca9685.set_pwm_freq(FREQUENCY)
+    pca9685.set_pwm_freq(FREQUENCY)
 
     # Set all thrusters to zero
-    # pca9685.set_pwm(0, 0, NEUTRAL_PULSE_WIDTH)
-    # pca9685.set_pwm(1, 0, NEUTRAL_PULSE_WIDTH)
-    # pca9685.set_pwm(2, 0, NEUTRAL_PULSE_WIDTH)
-    # pca9685.set_pwm(3, 0, NEUTRAL_PULSE_WIDTH)
-    # pca9685.set_pwm(4, 0, NEUTRAL_PULSE_WIDTH)
-    # pca9685.set_pwm(5, 0, NEUTRAL_PULSE_WIDTH)
-
-    # Create a message object
-
+    pca9685.set_pwm(0, 0, NEUTRAL_PULSE_WIDTH)
+    pca9685.set_pwm(1, 0, NEUTRAL_PULSE_WIDTH)
+    pca9685.set_pwm(2, 0, NEUTRAL_PULSE_WIDTH)
+    pca9685.set_pwm(3, 0, NEUTRAL_PULSE_WIDTH)
+    pca9685.set_pwm(4, 0, NEUTRAL_PULSE_WIDTH)
+    pca9685.set_pwm(5, 0, NEUTRAL_PULSE_WIDTH)
 
 if __name__ == '__main__':
     rospy.init_node('ThrusterPwmNode', anonymous=False)
@@ -70,5 +65,4 @@ if __name__ == '__main__':
     rospy.Subscriber('thruster_forces', ThrusterForces, callback)
 
     print "motor_interface: Launching node ThrusterPwmNode"
-
     rospy.spin()

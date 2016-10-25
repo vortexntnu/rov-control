95aca6b13920ca6746297b8744f151f62f162de9
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-10-25 22:21:09 +0200

    Remove functions moved out into helper

--- uranus_dp/src/allocator/pseudoinverse_allocator.cpp
+++ uranus_dp/src/allocator/pseudoinverse_allocator.cpp
@@ -1,48 +1,6 @@
 // See Fossen 2011, chapter 12.3.2
 #include "pseudoinverse_allocator.h"
 
-void printEigen(std::string name, const Eigen::MatrixXd &x)
-{
-  std::stringstream ss;
-  ss << name << " = " << std::endl << x;
-  ROS_INFO_STREAM(ss.str());
-}
-
-Eigen::MatrixXd getMatrixParam(ros::NodeHandle nh, std::string name)
-{
-  XmlRpc::XmlRpcValue matrix;
-  nh.getParam(name, matrix);
-
-  try
-  {
-    const int rows = matrix.size();
-    const int cols = matrix[0].size();
-    Eigen::MatrixXd X(rows,cols);
-    for (int i = 0; i < rows; ++i)
-      for (int j = 0; j < cols; ++j)
-        X(i,j) = matrix[i][j];
-    return X;
-  }
-  catch(...)
-  {
-    ROS_ERROR("Error in getMatrixParam. Returning 1-by-1 zero matrix.");
-    return Eigen::MatrixXd::Zero(1,1);
-  }
-}
-
-Eigen::MatrixXd pinv(const Eigen::MatrixXd &X)
-{
-  Eigen::MatrixXd X_pinv = X.transpose() * ( X*X.transpose() ).inverse();
-
-  if (isFucked(X_pinv))
-  {
-    ROS_WARN("Could not compute pseudoinverse. Returning transpose.");
-    return X.transpose();
-  }
-
-  return X_pinv;
-}
-
 PseudoinverseAllocator::PseudoinverseAllocator()
 {
   sub = nh.subscribe("rov_forces", 10, &PseudoinverseAllocator::callback, this);

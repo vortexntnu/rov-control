eb938f79ed06fbbcd8e89ee1c2abd1bdf8cfa66a
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-10-30 15:26:51 +0100

    Update pseudoinverse allocator
    Made variables lower_cased
    Added saturation of output
    Added parameter to detemine which DOFs to use
    Move parameter getting into separate function

--- uranus_dp/src/allocator/pseudoinverse_allocator.cpp
+++ uranus_dp/src/allocator/pseudoinverse_allocator.cpp
@@ -1,35 +1,64 @@
 // See Fossen 2011, chapter 12.3.2
 #include "pseudoinverse_allocator.h"
 
+#include <vector>
+#include <limits>
+#include <string>
+
 PseudoinverseAllocator::PseudoinverseAllocator()
 {
   sub = nh.subscribe("rov_forces", 10, &PseudoinverseAllocator::callback, this);
   pub = nh.advertise<vortex_msgs::ThrusterForces>("thruster_forces", 10);
 
-  if (!nh.getParam("/thrusters/dof", n))
-    ROS_FATAL("Failed to read parameter thrusters/dof.");
-  if (!nh.getParam("/thrusters/num", r))
-    ROS_FATAL("Failed to read parameter thrusters/dof.");
-  if (!getMatrixParam(nh, "thrusters/configuration_matrix", T))
-    ROS_ERROR("Failed to read thrust config matrix from param server.");
+  getParams();
 
   tau.setZero(n);
   u.setZero(r);
   K_inv.setIdentity(r,r);
 
-  if (!pinv(T, T_pinv))
+  if (!pseudoinverse(T, T_pinv))
     ROS_ERROR("Failed to compute pseudoinverse of thrust config matrix.");
 }
 
-void PseudoinverseAllocator::callback(const geometry_msgs::Wrench& tauMsg)
+void PseudoinverseAllocator::callback(const geometry_msgs::Wrench& tau_msg)
 {
-  tau << tauMsg.force.x,
-         tauMsg.force.y,
-         tauMsg.force.z,
-         tauMsg.torque.y,
-         tauMsg.torque.z;
+  {
+    int i = 0;
+    if (dofs["surge"])
+    {
+      tau(i) = tau_msg.force.x;
+      ++i;
+    }
+    if (dofs["sway"])
+    {
+      tau(i) = tau_msg.force.y;
+      ++i;
+    }
+    if (dofs["heave"])
+    {
+      tau(i) = tau_msg.force.z;
+      ++i;
+    }
+    if (dofs["roll"])
+    {
+      tau(i) = tau_msg.torque.x;
+      ++i;
+    }
+    if (dofs["pitch"])
+    {
+      tau(i) = tau_msg.torque.y;
+      ++i;
+    }
+    if (dofs["yaw"])
+    {
+      tau(i) = tau_msg.torque.z;
+      ++i;
+    }
+  }
 
   u = K_inv * T_pinv * tau;
+  if (!saturateVector(u, min_thrust, max_thrust))
+    ROS_WARN("Thrust vector u required saturation.");
 
   if (isFucked(u))
   {
@@ -37,7 +66,33 @@ void PseudoinverseAllocator::callback(const geometry_msgs::Wrench& tauMsg)
     return;
   }
 
-  vortex_msgs::ThrusterForces uMsg;
-  thrustEigenToMsg(u, uMsg);
-  pub.publish(uMsg);
+  vortex_msgs::ThrusterForces u_msg;
+  thrustEigenToMsg(u, u_msg);
+  pub.publish(u_msg);
+}
+
+void PseudoinverseAllocator::getParams()
+{
+  if (!nh.getParam("/thrusters/dof", n))
+    ROS_FATAL("Failed to read parameter thrusters/dof.");
+  if (!nh.getParam("/thrusters/num", r))
+    ROS_FATAL("Failed to read parameter thrusters/dof.");
+  if (!getMatrixParam(nh, "thrusters/configuration_matrix", T))
+    ROS_ERROR("Failed to read thrust config matrix from param server.");
+
+  std::vector<double> thrust;
+  if (!nh.getParam("/thrust", thrust))
+  {
+    min_thrust = -std::numeric_limits<double>::infinity();
+    max_thrust =  std::numeric_limits<double>::infinity();
+    ROS_ERROR_STREAM("Failed to read parameters min/max thrust. Defaulting to " << min_thrust << "/" << max_thrust << ".");
+  }
+  else
+  {
+    min_thrust = thrust.front();
+    max_thrust = thrust.back();
+  }
+
+  if (!nh.getParam("/control/dofs", dofs))
+    ROS_FATAL("Failed to read parameter controllable dofs map.");
 }
--- uranus_dp/src/allocator/pseudoinverse_allocator.h
+++ uranus_dp/src/allocator/pseudoinverse_allocator.h
@@ -9,26 +9,32 @@
 #include "geometry_msgs/Wrench.h"
 
 #include <Eigen/Dense>
-#include <vector>
+#include <map>
 
 class PseudoinverseAllocator
 {
 public:
   PseudoinverseAllocator();
-  void callback(const geometry_msgs::Wrench& tauMsg);
+  void callback(const geometry_msgs::Wrench& tau_msg);
 private:
   ros::NodeHandle nh;
   ros::Subscriber sub;
   ros::Publisher  pub;
 
+  double max_thrust;
+  double min_thrust;
+
   int n; // Number of control forces (length of tau)
-  int r; // Number of control inputs (length of f)
+  int r; // Number of control inputs (length of u)
+  std::map<std::string, bool> dofs; // Map of controllable dofs
 
   Eigen::VectorXd tau;    // (n) Control forces (forces and moments on the ROV)
   Eigen::VectorXd u;      // (r) Control inputs (forces for each thruster)
   Eigen::MatrixXd T;      // (n*r) Thrust configuration matrix
   Eigen::MatrixXd T_pinv; // (r*n) Generalized inverse of T
   Eigen::MatrixXd K_inv;  // (r*r) Inverse thrust coefficient matrix
+
+  void getParams();
 };
 
 #endif

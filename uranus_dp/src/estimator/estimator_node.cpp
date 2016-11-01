877fd895dda53fa817dc9561c71be77eb1d7d8d8
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-11-01 15:40:48 +0100

    Remove comments

--- uranus_dp/src/estimator/estimator_node.cpp
+++ uranus_dp/src/estimator/estimator_node.cpp
@@ -7,8 +7,6 @@ int main(int argc, char **argv)
   ROS_INFO("Launching node estimator.");
   ros::NodeHandle nh;
   SimpleEstimator e;
-  // IntegrationFilter estimator;
-  // ros::ServiceServer ss = nh.advertiseService("reset_estimator", &IntegrationFilter::reset, &estimator);
   ros::spin();
   return 0;
 }

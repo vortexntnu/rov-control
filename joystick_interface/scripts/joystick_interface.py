2dceadb34b7af7545f654589ea8f27c5d204d2fe
Sindre Hansen <sindrehansen92@gmail.com>
2016-11-02 03:19:41 +0100

    Move mapping and msg fields into separate entries
    The two lists buttons_map and axes_map, and the two dictionaries
    arm_msg_fields and motion_msg_fields should now be fairly easy to
    move to the parameter server.

--- joystick_interface/scripts/joystick_interface.py
+++ joystick_interface/scripts/joystick_interface.py
@@ -6,8 +6,8 @@ from sensor_msgs.msg import Joy
 class JoystickInterfaceNode(object):
     def __init__(self):
         rospy.init_node('joystick_node')
-        self.arm_cmd = vortex_msgs.msg.JoystickArmCommand()
-        self.motion_cmd = vortex_msgs.msg.JoystickMotionCommand()
+        self.arm_msg = vortex_msgs.msg.JoystickArmCommand()
+        self.motion_msg = vortex_msgs.msg.JoystickMotionCommand()
         self.sub = rospy.Subscriber('joy', Joy, self.callback, queue_size=10)
         self.pub_motion = rospy.Publisher('joystick_motion_command',
                                           vortex_msgs.msg.JoystickMotionCommand,
@@ -15,41 +15,58 @@ class JoystickInterfaceNode(object):
         self.pub_arm = rospy.Publisher('joystick_arm_command',
                                        vortex_msgs.msg.JoystickArmCommand,
                                        queue_size=10)
+
+        self.buttons_map = ['A', 'B', 'X', 'Y', 'LB', 'RB', 'back',
+                            'start', 'power', 'stick_button_left',
+                            'stick_button_right']
+
+        self.axes_map = ['horizontal_axis_left_stick',
+                         'vertical_axis_left_stick', 'LT',
+                         'horizontal_axis_right_stick',
+                         'vertical_axis_right_stick', 'RT',
+                         'dpad_horizontal', 'dpad_vertical']
+
     def callback(self, msg):
-        button_mapping = {
-            0: ('A', 'grip_close'),
-            1: ('B', 'grip_open'),
-            2: ('X', None),
-            3: ('Y', None),
-            4: ('LB', None),
-            5: ('RB', None),
-            6: ('back', 'control_mode'),
-            7: ('start', 'control_mode'),
-            8: ('power', None),
-            9: ('stick_button_left', None),
-            10: ('stick_button_right', None)
-        }
-        axes_mapping = {
-            0: ('horizontal_axis_left_stick', 'right'),
-            1: ('vertical_axis_left_stick', 'forward'),
-            2: ('LT', None),
-            3: ('horizontal_axis_right_stick', 'turn_right'),
-            4: ('vertical_axis_right_stick', 'tilt_up'),
-            5: ('RT', None),
-            6: ('cross_horizontal', None),
-            7: ('cross_vertical', None)
-        }
+        buttons = {}
+        axes = {}
 
         for i in range(len(msg.buttons)):
-            rospy.loginfo("%s: %i", button_mapping[i], msg.buttons[i])
-            if (button_mapping[i][1] is not None):
-                if (hasattr(self.arm_cmd, button_mapping[i][1])):
-                    setattr(self.arm_cmd, button_mapping[i][1], msg.buttons[i])
-                if (hasattr(self.motion_cmd, button_mapping[i][1])):
-                    setattr(self.motion_cmd, button_mapping[i][1], msg.buttons[i])
-        self.pub_arm.publish(self.arm_cmd)
-        self.pub_motion.publish(self.motion_cmd)
+            buttons[self.buttons_map[i]] = msg.buttons[i]
 
+        for j in range(len(msg.axes)):
+            axes[self.axes_map[j]] = msg.axes[j]
+
+        arm_msg_fields = {
+            'base_up' : axes['dpad_vertical'],
+            'base_down' : axes['dpad_vertical'] == -1,
+            'rot_left' : axes['dpad_horizontal'],
+            'rot_right' : axes['dpad_horizontal'] == -1,
+            'grip_open' : buttons['B'],
+            'grip_close' : buttons['A']
+        }
+
+        motion_msg_fields = {
+            'forward' : -axes['vertical_axis_left_stick'],
+            'right' : axes['horizontal_axis_left_stick'],
+            'down' : axes['RT'] - axes['LT'],
+            'tilt_up' : axes['vertical_axis_right_stick'],
+            'turn_right' : axes['horizontal_axis_right_stick']
+        }
+
+        for field, value in arm_msg_fields.iteritems():
+            setattr(self.arm_msg, field, value)
+
+        for field, value in motion_msg_fields.iteritems():
+            setattr(self.motion_msg, field, value)
+
+        #Control mode is separate to avoid overwriting old mode
+        if buttons['back']:
+            self.motion_msg.control_mode = 1
+        if buttons['start']:
+            self.motion_msg.control_mode = 0
+
+        self.pub_arm.publish(self.arm_msg)
+        self.pub_motion.publish(self.motion_msg)
 
 if __name__ == '__main__':
     try:

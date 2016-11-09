3d90e3d2d9a295ae3c41fb1c74f66dc6eb9e7f1b
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2016-11-09 21:39:42 +0100

    Rewrite motor_interface as a class

--- motor_interface/scripts/motor_interface.py
+++ motor_interface/scripts/motor_interface.py
@@ -3,50 +3,53 @@
 import rospy
 import Adafruit_PCA9685
 from numpy import interp
-
 from vortex_msgs.msg import ThrusterForces, ThrusterPwm
 
-BITS_PER_PERIOD               = 4096.0 # 12 bit PWM
-FREQUENCY                     = 249    # Max 400 Hz
-FREQUENCY_MEASURED            = 251.2  # Use this for better precision
-PERIOD_LENGTH_IN_MICROSECONDS = 1000000.0/FREQUENCY_MEASURED
+class MotorInterface(object):
+    def __init__(self):
+        self.BITS_PER_PERIOD               = 4096.0 # 12 bit PWM
+        self.FREQUENCY                     = 249    # Max 400 Hz
+        self.FREQUENCY_MEASURED            = 251.2  # Use this for better precision
+        self.PERIOD_LENGTH_IN_MICROSECONDS = 1000000.0/self.FREQUENCY_MEASURED
 
-T100_thrust      = rospy.get_param('/thrusters/characteristics/thrust')
-T100_pulse_width = rospy.get_param('/thrusters/characteristics/pulse_width')
-num_thrusters    = rospy.get_param('/propulsion/thrusters/num')
+        self.T100_thrust      = rospy.get_param('/thrusters/characteristics/thrust')
+        self.T100_pulse_width = rospy.get_param('/thrusters/characteristics/pulse_width')
+        self.num_thrusters    = rospy.get_param('/propulsion/thrusters/num')
 
-# Initialise the PCA9685 using the default address (0x40)
-pca9685 = Adafruit_PCA9685.PCA9685()
+        # Initialize the PCA9685 using the default address (0x40)
+        # self.pca9685 = Adafruit_PCA9685.PCA9685()
+        # pca9685.set_pwm_freq(FREQUENCY)
 
-def pulse_width_in_bits(force):
-    pulse_width_in_microseconds = interp(force, T100_thrust, T100_pulse_width)
-    normalized_duty_cycle = pulse_width_in_microseconds/PERIOD_LENGTH_IN_MICROSECONDS
-    return int(round(BITS_PER_PERIOD * normalized_duty_cycle))
+        # Initialize outputs to zero newton
+        neutral_pulse_width = self.pulse_width_in_bits(0)
+        # for i in range(num_thrusters):
+            # pca9685.set_pwm(i, 0, neutral_pulse_width)
 
-def callback(msg):
-    pwm_state = [None]*num_thrusters
-    for i in range(num_thrusters):
-        pwm_state[i] = pulse_width_in_bits(msg.thrust[i])
-        pca9685.set_pwm(i, 0, pwm_state[i])
+        rospy.init_node('motor_interface', anonymous=False)
+        self.pub = rospy.Publisher('pwm_state', ThrusterPwm, queue_size=10)
+        self.sub = rospy.Subscriber('thruster_forces', ThrusterForces, self.callback)
 
-    # Publish outputs for debug
-    pwm_msg = ThrusterPwm()
-    pwm_msg.pwm = pwm_state
-    pub.publish(pwm_state)
+        print 'Launching node motor_interface at', self.FREQUENCY, 'Hz'
+        rospy.spin()
 
-def init():
-    pca9685.set_pwm_freq(FREQUENCY)
+    def pulse_width_in_bits(self, force):
+        pulse_width_in_microseconds = interp(force, self.T100_thrust, self.T100_pulse_width)
+        normalized_duty_cycle = pulse_width_in_microseconds/self.PERIOD_LENGTH_IN_MICROSECONDS
+        return int(round(self.BITS_PER_PERIOD * normalized_duty_cycle))
 
-    neutral_pulse_width = pulse_width_in_bits(0)
-    for i in range(num_thrusters):
-        pca9685.set_pwm(i, 0, neutral_pulse_width)
+    def callback(self, msg):
+        pwm_state = [None]*self.num_thrusters
+        for i in range(self.num_thrusters):
+            pwm_state[i] = self.pulse_width_in_bits(msg.thrust[i])
+            # pca9685.set_pwm(i, 0, pwm_state[i])
+
+        # Publish outputs for debug
+        pwm_msg = ThrusterPwm()
+        pwm_msg.pwm = pwm_state
+        self.pub.publish(pwm_state)
 
 if __name__ == '__main__':
-    rospy.init_node('motor_interface', anonymous=False)
-    init()
-
-    pub = rospy.Publisher('pwm_state', ThrusterPwm, queue_size=10)
-    rospy.Subscriber('thruster_forces', ThrusterForces, callback)
-
-    print 'Launching node motor_interface at', FREQUENCY, 'Hz'
-    rospy.spin()
+    try:
+        motor_node = MotorInterface()
+    except rospy.ROSInterruptException:
+        pass

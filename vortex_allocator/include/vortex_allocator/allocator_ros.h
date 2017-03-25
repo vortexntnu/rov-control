a91325d4a2239c1b8b1746f0e9e7cf3356537165
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2017-03-25 23:43:24 +0100

    Change indentation to 2 spaces

--- vortex_allocator/include/vortex_allocator/allocator_ros.h
+++ vortex_allocator/include/vortex_allocator/allocator_ros.h
@@ -14,24 +14,24 @@
 class Allocator
 {
 public:
-    explicit Allocator(ros::NodeHandle nh);
-    void callback(const geometry_msgs::Wrench &msg);
+  explicit Allocator(ros::NodeHandle nh);
+  void callback(const geometry_msgs::Wrench &msg);
 private:
-    ros::NodeHandle nh;
-    ros::Subscriber sub;
-    ros::Publisher  pub;
+  ros::NodeHandle nh;
+  ros::Subscriber sub;
+  ros::Publisher  pub;
 
-    int num_dof;
-    int num_thrusters;
-    std::map<std::string, bool> dofs;  // Map of controllable dofs
-    double min_thrust;
-    double max_thrust;
-    const double FORCE_RANGE_LIMIT = 100;
+  int num_dof;
+  int num_thrusters;
+  std::map<std::string, bool> dofs;  // Map of controllable dofs
+  double min_thrust;
+  double max_thrust;
+  const double FORCE_RANGE_LIMIT = 100;
 
-    PseudoinverseAllocator *pseudoinverse_allocator;
+  PseudoinverseAllocator *pseudoinverse_allocator;
 
-    Eigen::VectorXd rovForcesMsgToEigen(const geometry_msgs::Wrench &msg);
-    bool healthyWrench(const Eigen::VectorXd &v);
+  Eigen::VectorXd rovForcesMsgToEigen(const geometry_msgs::Wrench &msg);
+  bool healthyWrench(const Eigen::VectorXd &v);
 };
 
 #endif  // VORTEX_ALLOCATOR_ALLOCATOR_ROS_H

34c1b988876ec0a61941de6f42ca48eb96cf25c0
Sindre Hansen <sindrehansen92@gmail.com>
2017-04-21 17:16:43 +0200

    Add init for sensor

--- sensor_interface/scripts/ms5837_interface.py
+++ sensor_interface/scripts/ms5837_interface.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
+import ms5837
 
 
 class Ms5837InterfaceNode(object):
     def __init__(self):
         rospy.init_node('pressure_node')
-        # Stuff
+        self.ms5837 = ms5837.MS5837(model=ms5837.MODEL_30BA, bus=1)
+        if not self.ms5837.init():
+            rospy.logfatal('Failed to initialise MS5837! Is the sensor connected?')
+        else:
+            if not self.ms5837.read():
+                rospy.logfatal('Failed to read MS5837!')
+            else:
+                rospy.loginfo('Successfully initialised MS5837')
 
 
 if __name__ == '__main__':

b82498c8e7684125b398092d977a88ba776ee103
Sindre Hansen <sindrehansen92@gmail.com>
2017-05-02 13:08:02 +0200

    Initialise all pwm-pins to 0

--- pwm_interface/scripts/pca9685_interface.py
+++ pwm_interface/scripts/pca9685_interface.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 
+import Adafruit_PCA9685
 import rospy
+
+FREQUENCY = 249
 
 
 class Pca9685InterfaceNode(object):
     def __init__(self):
         rospy.init_node('pwm_node')
+        self.pca9685 = Adafruit_PCA9685.PCA9685()
+        self.pca9685.set_pwm_frequency(FREQUENCY)
+        self.pca9685.set_all_pwm(0, 0)
 
 
 if __name__ == '__main__':

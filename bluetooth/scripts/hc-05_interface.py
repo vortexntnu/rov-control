5529103f7beb09e831967d69d66a0d7c3fcd33af
Sindre Hansen <sindrehansen92@gmail.com>
2017-05-03 13:52:38 +0200

    Fix linter errors

--- bluetooth/scripts/hc-05_interface.py
+++ bluetooth/scripts/hc-05_interface.py
@@ -4,6 +4,7 @@ import serial
 import Adafruit_BBIO.UART as UART
 from vortex_msgs.msg import ContainerID
 
+
 class Hc05InterfaceNode(object):
     def __init__(self):
         rospy.init_node('bluetooth_node')
@@ -15,11 +16,11 @@ class Hc05InterfaceNode(object):
     def init_serial(self):
         UART.setup("UART4")
         self.ser = serial.Serial(port=self.port,
-                        baudrate=9600,
-                        bytesize=serial.EIGHTBITS,
-                        parity=serial.PARITY_NONE,
-                        stopbits=serial.STOPBITS_ONE,
-                        timeout=1)
+                                 baudrate=9600,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
+                                 timeout=1)
         if not self.ser.is_open:
             rospy.logwarn("Unable to open %s", self.ser)
 
@@ -49,6 +50,7 @@ class Hc05InterfaceNode(object):
             else:
                 break
         return bytes(line)
+
 
 if __name__ == '__main__':
     try:

fa847b04d5e571302a9a1c2433c94b8f9959678b
Sindre Hansen <sindrehansen92@gmail.com>
2017-05-03 12:04:02 +0200

    Change to correct serial port (ttyO0 -> ttyO4), remove binary -> ascii transformation

--- bluetooth/scripts/hc-05_interface.py
+++ bluetooth/scripts/hc-05_interface.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import rospy
 import serial
 import Adafruit_BBIO.UART as UART
@@ -7,7 +8,7 @@ import binascii
 class Hc05InterfaceNode(object):
     def __init__(self):
         rospy.init_node('bluetooth_node')
-        self.port = "/dev/tty04"
+        self.port = "/dev/ttyO4"
         self.init_serial()
         self.init_publisher()
         self.read_bluetooth_data()
@@ -20,20 +21,19 @@ class Hc05InterfaceNode(object):
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1)
-        if not self.ser.is_open():
+        if not self.ser.is_open:
             rospy.logwarn("Unable to open %s", self.ser)
 
     def init_publisher(self):
         self.pub_bluetooth = rospy.Publisher(
-            'bluetooth/data',
+            'bluetooth/container_id',
             String,
             queue_size=10)
 
     def read_bluetooth_data(self):
         while not rospy.is_shutdown():
-            BT_msg = _self._readline()
+            BT_msg = self._readline()
             if len(BT_msg) > 0:
-                BT_msg = binascii.b2a_uu(BT_msg)
                 self.pub_bluetooth.publish(BT_msg)
             rospy.Rate(10).sleep()
 

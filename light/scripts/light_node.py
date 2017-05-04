8408e00537a50044a9cedd6e923f33eed191d5c1
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-05-04 14:36:19 +0200

    Add subscriber, publisher and callback

--- light/scripts/light_node.py
+++ light/scripts/light_node.py
@@ -16,13 +16,29 @@ class LightNode(object):
         self.pub_pwm = rospy.Publisher('pwm', Pwm, queue_size=10)
         GPIO.setup(BT_LIGHT_PIN, GPIO.OUT)
         GPIO.setup(RAMAN_LIGHT_PIN, GPIO.OUT)
+        self.pin_map = {"bluetooth":BT_LIGHT_PIN,"raman":RAMAN_LIGHT_PIN}
 
     def callback(self, msg):
         if len(msg.light) == len(msg.on):
             self.light_control(msg)
 
     def light_control(self, msg):
-        pass
+        for i in range(len(msg.light)):
+            if msg.light[i] == "front_pwm":
+                pwm_msg = Pwm
+                Pwm.pins[0] = PWM_LIGHT_PIN
+                Pwm.on[0] = 0
+                if msg.on[i]:
+                    Pwm.off = FULL_CYCLE
+                else:
+                    Pwm.off = 0
+                self.pub_pwm.publish(pwm_msg)
+
+            else if msg.light[i] == "bluetooth" or msg.light[i] == "raman":
+                if msg.on[i]:
+                    GPIO.output(pin_map[msg.light[i]],GPIO.HIGH)
+                else:
+                    GPIO.output(pin_map[msg.light[i]],GPIO.LOW)
 
 
 if __name__ == '__main__':

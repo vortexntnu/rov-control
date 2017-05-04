205b354bf2c27040f849194f2aaaa5319fedffe8
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-05-04 12:55:47 +0200

    Add subscriber and callback

--- light/scripts/light_node.py
+++ light/scripts/light_node.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 import rospy
 import Adafruit_BBIO.GPIO as GPIO
+from vortex_msgs.msg import LightInput
+from vortex_msgs.msg import Pwm
+
+PWM_LIGHT_PIN = 9 # Placeholder
+FULL_CYCLE = 4096
+BT_LIGHT_PIN = "P8_10" # Placeholder
+RAMAN_LIGHT_PIN = "P8_12" # Placeholder
 
 class LightNode(object):
     def __init__(self):
         rospy.init_node('light_node')
+        self.sub = rospy.Subscriber('light', LightInput, self.callback, queue_size=10)
+        self.pub_pwm = rospy.Publisher('pwm', Pwm, queue_size=10)
+        GPIO.setup(BT_LIGHT_PIN, GPIO.OUT)
+        GPIO.setup(RAMAN_LIGHT_PIN, GPIO.OUT)
 
-    def init_publisher():
-        pass
+    def callback(self, msg):
+        if len(msg.light) == len(msg.on):
+            self.light_control(msg)
 
-    def init_subscriber():
+    def light_control(self, msg):
         pass
 
 

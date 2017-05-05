80e67ffa99cc911219b316b172d7c74e1ede5c50
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-05-05 23:49:09 +0200

    Add callback, set camera feed selection pins based on msg

--- camera_selection/scripts/camera_selection.py
+++ camera_selection/scripts/camera_selection.py
@@ -3,7 +3,10 @@ import rospy
 import Adafruit_BBIO.GPIO as GPIO
 from vortex_msgs.msg import CameraFeedSelection
 
-GPIO_PIN_MAP = rospy.get_param('/camera/pin_map')
+PIN_MAP_FEED0 = rospy.get_param('/camera/pin_map_feed0')
+PIN_MAP_FEED1 = rospy.get_param('/camera/pin_map_feed1')
+PIN_MAP_FEED2 = rospy.get_param('/camera/pin_map_feed2')
+PIN_MAP_LIST = [PIN_MAP_FEED0,PIN_MAP_FEED1,PIN_MAP_FEED2]
 
 class CameraSelection(object):
     def __init__(self):
@@ -11,15 +14,19 @@ class CameraSelection(object):
         self.cam_select_sub = rospy.Subscriber('camera_feed_selection',CameraFeedSelection, self.callback)
 
         #Set pin as output
-        for selection_pin in GPIO_PIN_MAP:
-            GPIO.setup(GPIO_PIN_MAP[selection_pin], GPIO.OUT)
+        for pin_list in PIN_MAP_LIST:
+            for pin in pin_list:
+                GPIO.setup(pin, GPIO.OUT)
 
-        #Set to ground
-        for selection_pin in GPIO_PIN_MAP:
-        	GPIO.output(GPIO_PIN_MAP[selection_pin], GPIO.LOW)
 
     def callback(self, msg):
-        pass
+        feed_pin_map = PIN_MAP_LIST[msg.feed]
+        for i, level in enumerate(msg.pin_values):
+            if level:
+                GPIO.output(feed_pin_map[i], GPIO.HIGH)
+            else
+                GPIO.output(feed_pin_map[i], GPIO.LOW)
+
 
 
 if __name__ == '__main__':

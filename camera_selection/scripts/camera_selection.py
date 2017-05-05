42238f4b399d4572cb4ec4988cd1fbb59d94e8e0
Fonstein <ingrfjon@online.no>
2017-05-05 21:00:42 +0200

    Update parameter names

--- camera_selection/scripts/camera_selection.py
+++ camera_selection/scripts/camera_selection.py
@@ -2,19 +2,19 @@
 import rospy
 import Adafruit_BBIO.GPIO as GPIO
 
-GPIO_PIN_MAP = rospy.get_param('/camera/gpio_pins')
+GPIO_PIN_MAP = rospy.get_param('/camera/pin_map')
 
 class CameraSelection(object):
     def __init__(self):
         rospy.init_node('camera_selection')
 
         #Set pin as output
-        for key in GPIO_PIN_MAP:
-            GPIO.setup(GPIO_PIN_MAP[key], GPIO.OUT)
+        for selection_pin in GPIO_PIN_MAP:
+            GPIO.setup(GPIO_PIN_MAP[selection_pin], GPIO.OUT)
 
         #Set to ground
-        for key in GPIO_PIN_MAP:
-        	GPIO.output(GPIO_PIN_MAP[light], GPIO.LOW)
+        for selection_pin in GPIO_PIN_MAP:
+        	GPIO.output(GPIO_PIN_MAP[selection_pin], GPIO.LOW)
 
 
 if __name__ == '__main__':

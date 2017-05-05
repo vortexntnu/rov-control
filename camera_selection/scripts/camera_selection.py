40eca59ad84bcd8e4d087e224d8a9cc9789212cc
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-05-05 23:08:02 +0200

    Add subscriber and (empty) callback

--- camera_selection/scripts/camera_selection.py
+++ camera_selection/scripts/camera_selection.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import rospy
 import Adafruit_BBIO.GPIO as GPIO
+from vortex_msgs.msg import CameraFeedSelection
 
 GPIO_PIN_MAP = rospy.get_param('/camera/pin_map')
 
 class CameraSelection(object):
     def __init__(self):
         rospy.init_node('camera_selection')
+        self.cam_select_sub = rospy.Subscriber('camera_feed_selection',CameraFeedSelection, self.callback)
 
         #Set pin as output
         for selection_pin in GPIO_PIN_MAP:
@@ -15,6 +17,9 @@ class CameraSelection(object):
         #Set to ground
         for selection_pin in GPIO_PIN_MAP:
         	GPIO.output(GPIO_PIN_MAP[selection_pin], GPIO.LOW)
+
+    def callback(self, msg):
+        pass
 
 
 if __name__ == '__main__':

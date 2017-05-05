2c24aada070fcf502483668924e86bda1a749024
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-05-05 20:39:52 +0200

    Modify to control lights from gui

--- light/scripts/light_node.py
+++ light/scripts/light_node.py
@@ -13,35 +13,26 @@ class LightNode(object):
         rospy.init_node('light_node')
         self.sub = rospy.Subscriber('toggle_light', LightInput, self.callback, queue_size=10)
         self.pub_pwm = rospy.Publisher('pwm', Pwm, queue_size=10)
-        self.light_state = {}
         for key in GPIO_PIN_MAP:
-            self.light_state[key] = False
             GPIO.setup(GPIO_PIN_MAP[key], GPIO.OUT)
-        for key in PWM_PIN_MAP:
-            self.light_state[key] = False
 
     def callback(self, msg):
         self.light_control(msg)
 
     def light_control(self, msg):
-        for light in msg.toggle_light:
+        for index, light in enumerate(msg.toggle_light):
             if light in PWM_PIN_MAP:
                 pwm_msg = Pwm()
                 Pwm.pins[0] = PWM_PIN_MAP[light]
                 Pwm.on[0] = 0
-                if not self.light_state[light]:
-                    Pwm.off = FULL_CYCLE
-                else:
-                    Pwm.off = 0
-                self.light_state[light] = not self.light_state[light]
+                Pwm.off = (FULL_CYCLE * msg.intensity[index]) // 100
                 self.pub_pwm.publish(pwm_msg)
 
             else if light in GPIO_PIN_MAP:
-                if not self.light_state[light]:
+                if msg.intensity[index] > 0:
                     GPIO.output(GPIO_PIN_MAP[light], GPIO.HIGH)
                 else:
                     GPIO.output(GPIO_PIN_MAP[light] ,GPIO.LOW)
-                self.light_state[light] = not self.light_state[light]
 
 
 if __name__ == '__main__':

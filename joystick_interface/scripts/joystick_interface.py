d5af632f0f5564e40a288358af8b8cd4db8ffbfa
Sindre Hansen <sindrehansen92@gmail.com>
2017-05-06 22:35:00 +0200

    Add the new control modes
    Also simplify how the messages are populated

--- joystick_interface/scripts/joystick_interface.py
+++ joystick_interface/scripts/joystick_interface.py
@@ -16,6 +16,7 @@ class JoystickInterfaceNode(object):
                                                Manipulator,
                                                queue_size=10)
 
+        # Name buttons and axes based on index from joy-node
         self.buttons_map = ['A', 'B', 'X', 'Y', 'LB', 'RB', 'back',
                             'start', 'power', 'stick_button_left',
                             'stick_button_right']
@@ -27,6 +28,7 @@ class JoystickInterfaceNode(object):
                          'dpad_horizontal', 'dpad_vertical']
 
     def callback(self, msg):
+        # Connect values to names in two dictionaries
         buttons = {}
         axes = {}
 
@@ -36,7 +38,12 @@ class JoystickInterfaceNode(object):
         for j in range(len(msg.axes)):
             axes[self.axes_map[j]] = msg.axes[j]
 
-        motion_msg_motion = [
+        manipulator_msg = Manipulator()
+        manipulator_msg.claw_position = axes['dpad_vertical']
+        manipulator_msg.valve_direction = axes['dpad_horizontal']
+
+        motion_msg = PropulsionCommand()
+        motion_msg.motion = [
             axes['vertical_axis_left_stick'],
             -axes['horizontal_axis_left_stick'],
             (buttons['RB'] - buttons['LB']),
@@ -45,28 +52,17 @@ class JoystickInterfaceNode(object):
             -axes['horizontal_axis_right_stick']
         ]
 
-        motion_msg_control_mode = [
-            (buttons['back'] == 1),
-            (buttons['start'] == 1)
+        motion_msg.control_mode = [
+            (buttons['A'] == 1),
+            (buttons['B'] == 1),
+            (buttons['X'] == 1),
+            (buttons['Y'] == 1)
         ]
 
-        self.manipulator_msg = Manipulator()
-        self.manipulator_msg.claw_position = axes['dpad_vertical']
-        self.manipulator_msg.valve_direction = axes['dpad_horizontal']
+        motion_msg.header.stamp = rospy.get_rostime()
 
-        self.motion_msg = PropulsionCommand()
-        self.motion_msg.motion = [None]*6
-        for i in range(6):
-            self.motion_msg.motion[i] = motion_msg_motion[i]
-
-        self.motion_msg.control_mode = [None]*2
-        for i in range(2):
-            self.motion_msg.control_mode[i] = motion_msg_control_mode[i]
-
-        self.motion_msg.header.stamp = rospy.get_rostime()
-
-        self.pub_arm.publish(self.arm_msg)
-        self.pub_motion.publish(self.motion_msg)
+        self.pub_manipulator.publish(manipulator_msg)
+        self.pub_motion.publish(motion_msg)
 
 
 if __name__ == '__main__':

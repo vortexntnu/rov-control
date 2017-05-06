b798a765bcec0a7f2f7957e7b4b7463d97b02272
Sindre Hansen <sindrehansen92@gmail.com>
2017-05-06 22:17:20 +0200

    Use new manipulator message

--- joystick_interface/scripts/joystick_interface.py
+++ joystick_interface/scripts/joystick_interface.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 import rospy
-import vortex_msgs.msg
+from vortex_msgs.msg import PropulsionCommand, Manipulator
 from sensor_msgs.msg import Joy
 
 
 class JoystickInterfaceNode(object):
     def __init__(self):
         rospy.init_node('joystick_node')
-        self.arm_msg = vortex_msgs.msg.JoystickArmCommand()
-        self.motion_msg = vortex_msgs.msg.PropulsionCommand()
+
         self.sub = rospy.Subscriber('joy', Joy, self.callback, queue_size=10)
         self.pub_motion = rospy.Publisher('propulsion_command',
-                                          vortex_msgs.msg.PropulsionCommand,
+                                          PropulsionCommand,
                                           queue_size=10)
-        self.pub_arm = rospy.Publisher('joystick_arm_command',
-                                       vortex_msgs.msg.JoystickArmCommand,
-                                       queue_size=10)
+        self.pub_manipulator = rospy.Publisher('manipulator',
+                                               Manipulator,
+                                               queue_size=10)
 
         self.buttons_map = ['A', 'B', 'X', 'Y', 'LB', 'RB', 'back',
                             'start', 'power', 'stick_button_left',
@@ -37,12 +36,6 @@ class JoystickInterfaceNode(object):
         for j in range(len(msg.axes)):
             axes[self.axes_map[j]] = msg.axes[j]
 
-        arm_msg_fields = [
-            axes['dpad_vertical'],
-            -axes['dpad_horizontal'],
-            (buttons['A'] - buttons['B'])
-        ]
-
         motion_msg_motion = [
             axes['vertical_axis_left_stick'],
             -axes['horizontal_axis_left_stick'],
@@ -56,10 +49,12 @@ class JoystickInterfaceNode(object):
             (buttons['back'] == 1),
             (buttons['start'] == 1)
         ]
-        self.arm_msg.manipulator = [None]*3
-        for i in range(3):
-            self.arm_msg.manipulator[i] = arm_msg_fields[i]
 
+        self.manipulator_msg = Manipulator()
+        self.manipulator_msg.claw_position = axes['dpad_vertical']
+        self.manipulator_msg.valve_direction = axes['dpad_horizontal']
+
+        self.motion_msg = PropulsionCommand()
         self.motion_msg.motion = [None]*6
         for i in range(6):
             self.motion_msg.motion[i] = motion_msg_motion[i]
@@ -72,6 +67,7 @@ class JoystickInterfaceNode(object):
 
         self.pub_arm.publish(self.arm_msg)
         self.pub_motion.publish(self.motion_msg)
+
 
 if __name__ == '__main__':
     try:

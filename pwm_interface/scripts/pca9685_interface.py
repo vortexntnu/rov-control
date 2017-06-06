f200c99f46ed26be4cf128a05b8b96b1757373ed
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-06-06 21:41:09 +0200

    Move conversion from microsec to bit to pca9685_interface

--- pwm_interface/scripts/pca9685_interface.py
+++ pwm_interface/scripts/pca9685_interface.py
@@ -4,7 +4,12 @@ import Adafruit_PCA9685
 import rospy
 from vortex_msgs.msg import Pwm
 
+# Constants
+PWM_BITS_PER_PERIOD = rospy.get_param('/pwm/counter/max')
 FREQUENCY = rospy.get_param('/pwm/frequency/set')
+FREQUENCY_MEASURED = rospy.get_param('/pwm/frequency/measured')
+PERIOD_LENGTH_IN_MICROSECONDS = 1000000.0 / FREQUENCY_MEASURED
+PWM_ON = 0 # Start of duty cycle
 
 
 class Pca9685InterfaceNode(object):
@@ -20,7 +25,11 @@ class Pca9685InterfaceNode(object):
     def callback(self, msg):
         if len(msg.pins) == len(msg.on) == len(msg.off):
             for i in range(len(msg.pins)):
-                self.pca9685.set_pwm(msg.pins[i], msg.on[i], msg.off[i])
+                self.pca9685.set_pwm(msg.pins[i], PWM_ON, self.microsecs_to_bits(msg.off[i]))
+
+    def microsecs_to_bits(self, microsecs):
+        duty_cycle_normalized = microsecs / PERIOD_LENGTH_IN_MICROSECONDS
+        return int(round(PWM_BITS_PER_PERIOD * duty_cycle_normalized))
 
 
 if __name__ == '__main__':

9a365baa850e08f298ffd13eab97c47a6260018d
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2017-06-15 17:04:21 +0200

    Refactor thruster interface
    Remove rate limiter and get offset from server

--- thruster_interface/scripts/thruster_interface_node.py
+++ thruster_interface/scripts/thruster_interface_node.py
@@ -9,11 +9,10 @@ from thruster_interface.srv import ThrustersEnable, ThrustersEnableResponse
 
 THRUST_RANGE_LIMIT = 100
 
+NUM_THRUSTERS = rospy.get_param('/propulsion/thrusters/num')
+THRUST_OFFSET = rospy.get_param('/thrusters/offset')
 LOOKUP_THRUST = rospy.get_param('/thrusters/characteristics/thrust')
 LOOKUP_PULSE_WIDTH = rospy.get_param('/thrusters/characteristics/pulse_width')
-NUM_THRUSTERS = rospy.get_param('/propulsion/thrusters/num')
-MAX_RATE = rospy.get_param('/thrusters/rate_of_change/max')
-RATE_LIMITING_ENABLED = rospy.get_param('/thruster_interface/rate_limiting_enabled')
 THRUSTERS_CONNECTED = rospy.get_param('/thruster_interface/thrusters_connected')
 THRUSTER_PWM_PINS = rospy.get_param('/pwm/pins/thrusters')
 
@@ -24,14 +23,6 @@ class ThrusterInterface(object):
         self.pub_pwm = rospy.Publisher('pwm', Pwm, queue_size=10)
         self.sub = rospy.Subscriber('thruster_forces', Float64ArrayStamped, self.callback)
         self.srv = rospy.Service('/thruster_interface/thrusters_enable', ThrustersEnable, self.handle_thrusters_enable)
-
-        self.prev_time = rospy.get_rostime()
-        self.is_initialized = False
-
-        # The setpoint is the desired value (input)
-        self.thrust_setpoint = [0] * NUM_THRUSTERS
-        # The reference is the output value (rate limited)
-        self.thrust_reference = [0] * NUM_THRUSTERS
 
         self.thrusters_enabled = True
 
@@ -51,26 +42,8 @@ class ThrusterInterface(object):
     def callback(self, msg):
         if not self.healthy_message(msg):
             return
-
-        if not self.is_initialized:
-            self.prev_time = msg.header.stamp
-            self.is_initialized = True
-            rospy.loginfo('Successfully initialized.')
-            return
-
-        curr_time = msg.header.stamp
-        dt = (curr_time - self.prev_time).to_sec()
-        if (dt <= 0) and RATE_LIMITING_ENABLED:
-            rospy.logwarn_throttle(10, 'Zero time difference between messages, ignoring...')
-            return
-
-        self.prev_time = curr_time
-
-        thrust_setpoint_tuple = msg.data
-        self.thrust_setpoint = list(thrust_setpoint_tuple)
-
-        self.update_reference(dt)
-        self.set_pwm()
+        thrust = list(msg.data)
+        self.set_pwm(thrust)
 
     def handle_thrusters_enable(self, req):
         if req.thrusters_enable:
@@ -85,24 +58,12 @@ class ThrusterInterface(object):
     def thrust_to_microsecs(self, thrust):
         return interp(thrust, LOOKUP_THRUST, LOOKUP_PULSE_WIDTH)
 
-    def update_reference(self, dt):
-        if RATE_LIMITING_ENABLED:
-            rate_of_change = (self.thrust_setpoint - self.thrust_reference) / dt
-            for i in range(NUM_THRUSTERS):
-                if rate_of_change[i] > MAX_RATE:
-                    self.thrust_reference[i] += dt * MAX_RATE
-                elif rate_of_change[i] < -MAX_RATE:
-                    self.thrust_reference[i] -= dt * MAX_RATE
-                else:
-                    self.thrust_reference[i] = self.thrust_setpoint[i]
-        else:
-            self.thrust_reference = self.thrust_setpoint
-
-    def set_pwm(self):
+    def set_pwm(self, thrust):
         microsecs = [None] * NUM_THRUSTERS
         pwm_msg = Pwm()
+
         for i in range(NUM_THRUSTERS):
-            microsecs[i] = self.thrust_to_microsecs(self.thrust_reference[i])
+            microsecs[i] = self.thrust_to_microsecs(thrust[i] + THRUST_OFFSET[i])
             pwm_msg.pins.append(THRUSTER_PWM_PINS[i])
             pwm_msg.positive_width_us.append(microsecs[i])
         if THRUSTERS_CONNECTED and self.thrusters_enabled:

dceb24fa3ad6b18a9e87e15a7a49e9b61eaa4bed
Sindre Hansen <sindrehansen92@gmail.com>
2018-05-15 16:25:26 +0200

    PEP8ify the liftbag script

--- liftbag/scripts/liftbag.py
+++ liftbag/scripts/liftbag.py
@@ -7,28 +7,26 @@ from std_msgs.msg import String
 
 
 class LiftbagNode(object):
-	def __init__(self):
-		rospy.init_node('liftbag_node')
-		self.sub = rospy.Subscriber('liftbag_release', String, self.callback)
+    def __init__(self):
+        rospy.init_node('liftbag_node')
+        self.sub = rospy.Subscriber('liftbag_release', String, self.callback)
 
+    def callback(self, msg):
+        # Creating a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def callback(self, msg): 
-		#Creating a TCP/IP socket
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connecting the socket to the port where ESP8266 is listening
+        server_address = ('192.168.4.1', 81)
+        print >>sys.stderr, 'connectin to %s port %s' % server_address
+        sock.connect(server_address)
 
-		#Connecting the socket to the port where ESP8266 is listening
-		server_address = ('192.168.4.1', 81)
-		print >>sys.stderr, 'connectin to %s port %s' % server_address
-		sock.connect(server_address)
-
-		print >>sys.stderr, 'closing socket'
-		sock.close()
-
+        print >>sys.stderr, 'closing socket'
+        sock.close()
 
 
 if __name__ == '__main__':
-	try:
-		liftbag_node = LiftbagNode()
-		rospy.spin()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        liftbag_node = LiftbagNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
